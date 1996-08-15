19a219115455b2a0a0fb9f27b601cf3df649b4e8
SidWright <SidWright@8bb03f63-af10-0410-889a-a89e84ef1bc8>
1996-08-15 00:42:51 +0000

    Patch 7

--- trunk/apsim/millet/source/mil_main.for
+++ trunk/apsim/millet/source/mil_main.for
@@ -10,8 +10,8 @@
 
 *   Notes:
 *   $Log$
-*   Revision 1.4  1997/01/07 03:05:15  SidWright
-*   Patch 6
+*   Revision 1.5  1997/01/08 03:40:32  SidWright
+*   Patch 7
 *r  $
 *      
 *         Rev 1.7   19 Jul 1995 18:21:16   PVCSUSER
