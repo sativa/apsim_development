d260b0eedb3287b7b00892db0cc75691e9600dfb
nobody <nobody@8bb03f63-af10-0410-889a-a89e84ef1bc8>
1997-01-03 00:22:22 +0000

    This commit was manufactured by cvs2svn to create tag 'Rel1_3p4'.

--- apsim/millet/source/mil_main.for
+++ apsim/millet/source/mil_main.for
@@ -10,8 +10,8 @@
 
 *   Notes:
 *   $Log$
-*   Revision 1.8  1996/08/15 00:42:54  WrightS
-*   Patch 10
+*   Revision 1.2  1997/01/02 02:47:42  SidWright
+*   Patch 4
 *r  $
 *      
 *         Rev 1.7   19 Jul 1995 18:21:16   PVCSUSER
