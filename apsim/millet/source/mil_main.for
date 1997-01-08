9810b9dbd73eaf9049a2335f3e18c8a1d3f2bde4
nobody <nobody@8bb03f63-af10-0410-889a-a89e84ef1bc8>
1997-01-08 04:35:09 +0000

    This commit was manufactured by cvs2svn to create tag 'Rel1_3p7'.

--- apsim/millet/source/mil_main.for
+++ apsim/millet/source/mil_main.for
@@ -10,8 +10,8 @@
 
 *   Notes:
 *   $Log$
-*   Revision 1.8  1996/08/15 00:42:54  WrightS
-*   Patch 10
+*   Revision 1.5  1997/01/08 03:40:32  SidWright
+*   Patch 7
 *r  $
 *      
 *         Rev 1.7   19 Jul 1995 18:21:16   PVCSUSER
