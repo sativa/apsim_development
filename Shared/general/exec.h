53cd77ae4e6cc770f82d241fb6527aa763abae37
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
1997-06-19 01:46:09 +0000

    Initial revision

---
+++ Shared/general/exec.h
@@ -0,0 +1,16 @@
+
+// ------------------------------------------------------------------
+//  Short description:
+//    executes a program and optionally waits until it has finished.
+
+//  Notes:
+
+//  Changes:
+//    DPH 17/4/1997
+
+// ------------------------------------------------------------------
+void Exec(const char* Command_line,
+          int Show_flag,
+          const char* Window_title,
+          bool Wait_for_finish);
+
