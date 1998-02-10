a6bf36e6827f19f0918411cff66e2155fe058a73
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
1998-02-10 05:55:44 +0000

    new files

---
+++ trunk/Shared/general/stream_processor.h
@@ -0,0 +1,58 @@
+//---------------------------------------------------------------------------
+#ifndef stream_processorH
+#define stream_processorH
+
+#include <general\mystring.h>
+#include <general\mylist.h>
+
+class Stream_processor;
+// ------------------------------------------------------------------
+//  Short description:
+//     Base class for line processor
+
+//  Notes:
+
+//  Changes:
+//    DPH 8/10/97
+
+// ------------------------------------------------------------------
+class Line_processor
+   {
+   protected:
+      char Macro_char;
+      virtual bool Get_macro_value (const char* Macro_name, string& Macro_value) {return true;};
+
+   public:
+      virtual bool Replace_macros (string& Line);
+
+   friend Stream_processor;
+   };
+
+// ------------------------------------------------------------------
+//  Short description:
+//     this class takes an input stream and writes the contents to
+//     an output stream.  For each line, it calls the specified line
+//     processor to give it the opportunity to change the line or
+//     to exclude from the out stream.
+
+//  Notes:
+
+//  Changes:
+//    DPH 8/10/97
+
+// ------------------------------------------------------------------
+class Stream_processor
+   {
+   public:
+      Stream_processor (void);
+
+      void Add_processor (Line_processor* Processor)
+         {Processors.push_back (Processor);}
+
+      void Go (istream& In_stream,
+               ostream& Out_stream);
+
+   private:
+      list<Line_processor*> Processors;
+   };
+#endif
