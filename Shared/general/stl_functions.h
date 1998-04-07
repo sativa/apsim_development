6292dbfd40ee43e69b16e1d1e591000eb074dc3f
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
1998-04-07 22:35:47 +0000

    added vector string to double conversion routines

--- Shared/general/stl_functions.h
+++ Shared/general/stl_functions.h
@@ -2,6 +2,8 @@
 #ifndef STL functionsH
 #define STL functionsH
 
+#include <functional>
+#include <general\string_functions.h>
 // ------------------------------------------------------------------
 //  Short description:
 //    locates an object in a container and returns the numerical
@@ -69,6 +71,64 @@ void Destroy_pointers (CT& container, const T )
       container.pop_back();
 		delete Ptr;
 		}
+   }
+
+template <class CT, class T>
+class string2double_and_store : public std::unary_function<T, void>
+   {
+   private:
+      CT& container;
+   public:
+      string2double_and_store (CT& c) : container(c) {}
+      void operator() (T& arg)
+         {container.push_back (atof(arg.c_str()));}
+   };
+
+template <class CT, class T>
+class double2string_and_store : public std::unary_function<T, void>
+   {
+   private:
+      CT& container;
+   public:
+      double2string_and_store (CT& c) : container(c) {}
+      void operator() (T& arg)
+         {container.push_back (ftoa(arg, 5));}
+   };
+
+// ------------------------------------------------------------------
+//  Short description:
+//    convert a container of strings to a container of doubles.
+
+//  Notes:
+
+//  Changes:
+//    DPH 17/4/1997
+
+// ------------------------------------------------------------------
+template <class CT1, class CT2>
+void String2double (CT1& source, CT2& dest)
+   {
+   dest.erase(dest.begin(), dest.end());
+   string2double_and_store<CT2, string> convert(dest);
+   std::for_each(source.begin(), source.end(), convert);
+   }
+
+// ------------------------------------------------------------------
+//  Short description:
+//    convert a container of strings to a container of doubles.
+
+//  Notes:
+
+//  Changes:
+//    DPH 17/4/1997
+
+// ------------------------------------------------------------------
+template <class CT1, class CT2>
+void Double2string (CT1& source, CT2& dest)
+   {
+   dest.erase(dest.begin(), dest.end());
+   double2string_and_store<CT2, double> convert(dest);
+   std::for_each(source.begin(), source.end(), convert);
    }
 
 #endif
