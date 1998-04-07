92750e2f6488c73713120535cdb10084590454f5
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
1998-04-07 22:37:02 +0000

    new TDataSet manipulation routines.

--- trunk/Shared/general/vcl_functions.h
+++ trunk/Shared/general/vcl_functions.h
@@ -2,6 +2,8 @@
 #define vclfunctionsH
 
 #include "Grids.hpp"
+#include <fstream.h>
+#include <vcl\db.hpp>
 
 // ------------------------------------------------------------------
 //  Short description:
@@ -31,6 +33,7 @@ void TStrings_2_stl (TStrings* VCL_list, STL_container& ls)
 
 //  Changes:
 //    DPH 17/4/1997
+//    dph 17/3/98 removed reference to list - defect.
 
 // ------------------------------------------------------------------
 template <class STL_container>
@@ -39,9 +42,9 @@ void Stl_2_tstrings (STL_container& ls, TStrings* VCL_list)
    VCL_list->Clear();
 
    // loop through all field names.
-   for (list<string>::iterator Iter = ls.begin();
-                               Iter != ls.end();
-                               Iter++)
+   for (STL_container::iterator Iter = ls.begin();
+                                Iter != ls.end();
+                                Iter++)
       VCL_list->Add ((*Iter).c_str());
    }
 
@@ -80,5 +83,95 @@ void Grid_output_to_csv (TStringGrid* grid, ostream& csv_stream);
 
 // ------------------------------------------------------------------
 void Grid_clear (TStringGrid* grid);
+
+// ------------------------------------------------------------------
+//  Short description:
+//      retrieve a list of names of all active datasets in a component
+
+//  Notes:
+
+//  Changes:
+//    DPH 5/2/98
+
+// ------------------------------------------------------------------
+void Get_active_datasets(TComponent* component, TStrings* Dataset_names);
+
+// ------------------------------------------------------------------
+//  Short description:
+//      retrieve a list of names of all active dataset names in component
+
+//  Notes:
+
+//  Changes:
+//    DPH 5/2/98
+
+// ------------------------------------------------------------------
+void Get_active_databases(TComponent* component, TStrings* Database_names);
+
+// ------------------------------------------------------------------
+//  Short description:
+//      retrieve a specific active dataset on specified component.
+
+//  Notes:
+
+//  Changes:
+//    DPH 5/2/98
+
+// ------------------------------------------------------------------
+TDataSet* Get_active_dataset(TComponent* component, const char* Dataset_name);
+
+// ------------------------------------------------------------------
+//  Short description:
+//      return a list of tables for the specified databases.  The database
+//      names passed in are database aliases.
+
+//  Notes:
+
+//  Changes:
+//    DPH 5/2/98
+
+// ------------------------------------------------------------------
+void Get_tables_from_databases (TStringList* Database_names,
+                                TStringList* Table_names);
+
+// ------------------------------------------------------------------
+//  Short description:
+//      retrieve a specified component from a parent component
+
+//  Notes:
+
+//  Changes:
+//    DPH 5/2/98
+
+// ------------------------------------------------------------------
+TComponent* Locate_component(TComponent* component, const char* Component_name);
+
+
+// ------------------------------------------------------------------
+//  Short description:
+//      retrieve a list of all field names in a specified dataset.
+
+//  Notes:
+
+//  Changes:
+//    DPH 5/2/98
+
+// ------------------------------------------------------------------
+void Get_field_list (TDataSet* dataset, TStringList* field_names);
+
+// ------------------------------------------------------------------
+//  Short description:
+//      copy the structure from one dataset to another for those
+//      fields specified.
+
+//  Notes:
+
+//  Changes:
+//    DPH 5/2/98
+
+// ------------------------------------------------------------------
+void Copy_dataset_structure (TDataSet* source,
+                             TDataSet* destination,
+                             TStringList* field_names);
 
 #endif
