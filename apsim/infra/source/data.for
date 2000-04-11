2beefb06b8483949b10ea4916e89c414f3cff8b6
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2000-04-11 23:00:00 +0000

    removed a unwanted debug print statement

--- apsim/infra/source/data.for
+++ apsim/infra/source/data.for
@@ -297,7 +297,6 @@
  
       else if (value + margin_val.lt.lower - margin_lower) then
          e_messg = blank
-         print *, 'vname=',vname
          write (e_messg, '(2a, g16.7e2, 2a, g16.7e2)')
      :                      trim(vname)
      :                    , ' = '
