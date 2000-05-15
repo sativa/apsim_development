3efac780925adbccce22e5dbe70a14c56416f4a4
deneh <deneh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2000-05-15 05:47:56 +0000

    Changes made for DamEasy prototype

--- trunk/ApsimOutlook/Source/Outlook/TPreferences_form.cpp
+++ trunk/ApsimOutlook/Source/Outlook/TPreferences_form.cpp
@@ -13,6 +13,7 @@ TPreferences_form *Preferences_form;
 
 static const char* SOI_SECTION = "soi";
 static const char* SOI_FILE_KEY_WORD = "soi file";
+static const char* OPTIONS_SECTION = "options";
 //---------------------------------------------------------------------------
 __fastcall TPreferences_form::TPreferences_form(TComponent* Owner)
    : TForm(Owner)
@@ -28,8 +29,11 @@ void __fastcall TPreferences_form::FormShow(TObject *Sender)
    Ini.Set_file_name(p.Get_path().c_str());
    string SOI_file_name;
    Ini.Read (SOI_SECTION, SOI_FILE_KEY_WORD, SOI_file_name);
-
    File_name_edit->Text = SOI_file_name.c_str();
+
+   string St;
+   Ini.Read (OPTIONS_SECTION, "colour_background", St);
+   ColourBackgroundCheckbox->Checked = !Str_i_Eq(St, "off");
    }
 //---------------------------------------------------------------------------
 void __fastcall TPreferences_form::FormClose(TObject *Sender,
@@ -43,6 +47,11 @@ void __fastcall TPreferences_form::FormClose(TObject *Sender,
       Ini_file Ini;
       Ini.Set_file_name(p.Get_path().c_str());
       Ini.Write (SOI_SECTION, SOI_FILE_KEY_WORD, File_name_edit->Text.c_str());
-      }
+      if (ColourBackgroundCheckbox->Checked)
+         Ini.Write(OPTIONS_SECTION, "colour_background", "on");
+      else
+         Ini.Write(OPTIONS_SECTION, "colour_background", "off");
+
+     }
    }
-//---------------------------------------------------------------------------
+
