c8aee089f13dcdda86590b6c7154cc25061f3dcf
deneh <deneh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2000-08-24 02:50:42 +0000

    A few lines shuffled - no significant change

--- ApsimOutlook/Source/Outlook/ApsimOutlook.cpp
+++ ApsimOutlook/Source/Outlook/ApsimOutlook.cpp
@@ -4,18 +4,18 @@
 #include "TSplashForm.h"
 #include "TSkin.h"
 USERES("ApsimOutlook.res");
-USEFORM("TPreferences_form.cpp", Preferences_form);
+USEFORM("about.cpp", AboutBox);
 USEFORM("ChildWin.cpp", MDIChild);
 USEFORM("Main.cpp", MainForm);
+USEFORM("TChartSettingsForm.cpp", ChartSettingsForm);
 USEFORM("TDirectory_select_form.cpp", Directory_select_form);
 USEFORM("TDrill_down_form.cpp", Drill_down_form);
-USEFORM("about.cpp", AboutBox);
-USELIB("general.lib");
-USEFORM("TValueSelectionForm.cpp", ValueSelectionForm);
-USEFORM("TChartSettingsForm.cpp", ChartSettingsForm);
+USEFORM("TPreferences_form.cpp", Preferences_form);
+USEUNIT("TSkin.cpp");
 USEFORM("TSplashForm.cpp", SplashForm);
 USEFORM("TTabRenameForm.cpp", TabRenameForm);
-USEUNIT("TSkin.cpp");
+USEFORM("TValueSelectionForm.cpp", ValueSelectionForm);
+USELIB("K:\general\general.lib");
 //---------------------------------------------------------------------------
 AnsiString CommandLine;
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR cmdline, int)
@@ -33,12 +33,13 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR cmdline, int)
       Application->CreateHandle();
 
       Application->CreateForm(__classid(TMainForm), &MainForm);
-      Application->CreateForm(__classid(TPreferences_form), &Preferences_form);
-      Application->CreateForm(__classid(TDirectory_select_form), &Directory_select_form);
-      Application->CreateForm(__classid(TDrill_down_form), &Drill_down_form);
-      Application->CreateForm(__classid(TAboutBox), &AboutBox);
-      Application->CreateForm(__classid(TValueSelectionForm), &ValueSelectionForm);
-      Application->CreateForm(__classid(TTabRenameForm), &TabRenameForm);
+       Application->CreateForm(__classid(TAboutBox), &AboutBox);
+       Application->CreateForm(__classid(TDirectory_select_form), &Directory_select_form);
+       Application->CreateForm(__classid(TDrill_down_form), &Drill_down_form);
+       Application->CreateForm(__classid(TPreferences_form), &Preferences_form);
+       Application->CreateForm(__classid(TTabRenameForm), &TabRenameForm);
+       Application->CreateForm(__classid(TValueSelectionForm), &ValueSelectionForm);
+
       Application->Run();
    }
    catch (Exception &exception)
