25aaa80fd19f87218b47ad34b1154ec22144b05f
nobody <nobody@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2000-09-11 04:06:40 +0000

    This commit was manufactured by cvs2svn to create tag 'JNGH_110900'.

--- Shared/general/stristr.h
+++
@@ -1,8 +0,0 @@
-#include <general\general.h>
-
-char * GENERAL_EXPORT stristr(char *text_str, const char *srch_str);
-/*	If the string 'text_str' contains a substring equal to the
- * string 'srch_str' (case insensitive), then this function will return
- * a pointer to that substring.  Otherwise, NULL will be returned.
- */
-
