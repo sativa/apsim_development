9d056327ab93ca536bb63d2d588213deb8192b2a
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2000-11-14 00:01:41 +0000

    changes for new classes in APS32.dll

--- trunk/APSRun/Source/apsrun.cpp
+++ trunk/APSRun/Source/apsrun.cpp
@@ -1,61 +1,72 @@
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
-#include <aps\apsim_run_collection.h>
+
+#include <aps\APSIMSimulationCollection.h>
 #include <general\string_functions.h>
 #include <general\stristr.h>
 #include <fstream>
 #include <dos.h>
-#include <aps\apsuite.h>
-#include "TAPSIM_config_form.h"
+#include "TAPSIMRunForm.h"
 
 USERES("APSRun.res");
 USELIB("aps32.lib");
 USELIB("general.lib");
-USEFORM("TAPSIM_config_form.cpp", APSIM_config_form);
+USEFORM("TAPSIMRunForm.cpp", APSIMRunForm);
+using namespace std;
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR,  int)
    {
 // ------------------------------------------------------------------
 //  Short description:
-//    This application will be passed either a control file (.CON) or a
-//    run file (.RUN) depending on what the user has right clicked on.
+//    This application will be passed either a control file (.CON), a
+//    run file (.RUN), or a .SIM file depending on what the user has
+//    right clicked on.
 
 //  Notes:
 
 //  Changes:
 //    DPH 28/10/97
+//    dph 7/7/2000 added support for .sim files.
 
 // ------------------------------------------------------------------
    if (_argc == 2)
       {
-      APSIM_run_collection Run_collection;
+      APSIMSimulationCollection simulations;
 
       // Does the command line contain a control file?
       if (stristr(_argv[1], ".con") != NULL)
          {
          // yes - better ask user for a configuration.
-         TAPSIM_config_form* Form = new TAPSIM_config_form(NULL);
-         Form->Control_filename = _argv[1];
-         if (Form->ShowModal() == mrOk)
-            {
-            string DefaultRunFile = APSDirectories().Get_working() + "\\apsim.run";
-            Run_collection.Set_filename (DefaultRunFile.c_str());
-            Run_collection.Add("Default run", Form->Get_config(), Form->Get_simulations());
-            }
+         TAPSIMRunForm* runForm = new TAPSIMRunForm(NULL);
+         runForm->controlFilename = _argv[1];
+         if (runForm->ShowModal() == mrOk)
+            runForm->getSelectedSimulations(simulations);
          }
       else if (stristr(_argv[1], ".run") != NULL)
-         Run_collection.Set_filename (_argv[1]);
+         {
+         simulations = APSIMSimulationCollection(_argv[1]);
+         simulations.read();
+         }
 
-      // go perform run.
-      if (Run_collection.Count() > 0)
-         Run_collection.Run();
+      else if (stristr(_argv[1], ".sim") != NULL)
+         simulations.addSimulation(new APSIMSimulation(_argv[1]));
+
+      try
+         {
+         simulations.run();
+         }
+      catch (string& msg)
+         {
+         MessageBox(NULL, msg.c_str(), "APSIM Error", MB_ICONSTOP | MB_OK);
+         }
       }
    else
       {
-      MessageBox (NULL, "Usage:  APSRun [control filename | run filename]",
-                        "Error",
-                        MB_ICONSTOP | MB_OK);
+      MessageBox (NULL, "Usage:  APSRun [.con file | .run file | "
+                        ".sim file]",
+                  "Error",
+                  MB_ICONSTOP | MB_OK);
 
       }
    return 0;
