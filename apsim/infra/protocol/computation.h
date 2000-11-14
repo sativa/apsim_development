1f5a2bec89ec9b866c06e6ac8435a0bb0639c7e0
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2000-11-14 05:55:51 +0000

    Initial revision

---
+++ apsim/infra/protocol/computation.h
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+#ifndef computationH
+#define computationH
+#include "event.h"
+#include "protocolexport.h"
+#include "interfaces.h"
+// ------------------------------------------------------------------
+//  Short description:
+//    Encapsulates a component "computation".  A computation is a
+//    set of entry points into a component's calculation part.
+//    eg: init, term, action, inevent, outevent.
+                           
+//  Notes:
+
+//  Changes:
+//    dph 22/2/2000
+
+// ------------------------------------------------------------------
+class PROTOCOL_EXPORT PROTOCOLComputation : public IComputation
+   {
+   public:
+      PROTOCOLComputation(IComponent* component,
+                          const std::string& dllFileName,
+                          const std::string& ssdl);
+      ~PROTOCOLComputation(void);
+
+      virtual void init(void) { };
+      virtual IComponent* getComponent(void) const {return component;}
+
+      virtual void create(void) const;
+      virtual void init(void) const;
+      virtual void term(void) const;
+      virtual void action(PROTOCOLMessage& Event) const;
+      virtual void inEvent(PROTOCOLEvent& Event) const;
+      virtual void outEvent(PROTOCOLEvent& Event) const { };
+      virtual void complete(PROTOCOLMessage& Event) const { };
+      virtual void idle(void) const{ };
+   private:
+      PROTOCOLDLLInfo dllInfo;
+      IComponent* component;
+      int anInstanceNo;
+      std::string ssdl;
+
+   };
+#endif
