2374d07ada880d0246812ce571da600910536335
deanh <deanh@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2001-01-31 01:52:34 +0000

    updated for C++ Builder 5

--- trunk/tools/ContFileGen/Source/ContFileGen.cpp
+++ trunk/tools/ContFileGen/Source/ContFileGen.cpp
@@ -4,10 +4,7 @@
 //---------------------------------------------------------------------------
 USEFORM("TMain_form.cpp", Main_form);
 USERES("ContFileGen.res");
-USEUNIT("k:\general\path.cpp");
-USEUNIT("k:\general\string_functions.cpp");
-USEUNIT("k:\general\stream_functions.cpp");
-USEUNIT("k:\general\vcl_functions.cpp");
+USELIB("general.lib");
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -15,7 +12,7 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
    {
        Application->Initialize();
        Application->CreateForm(__classid(TMain_form), &Main_form);
-      Application->Run();
+       Application->Run();
    }
    catch (Exception &exception)
    {
