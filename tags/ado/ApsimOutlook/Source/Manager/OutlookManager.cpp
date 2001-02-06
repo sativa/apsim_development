974df1d840a513621a9978785740e434c281a961
nobody <nobody@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2001-02-06 06:46:09 +0000

    This commit was manufactured by cvs2svn to create tag 'ado'.

---
+++ tags/ado/ApsimOutlook/Source/Manager/OutlookManager.cpp
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#pragma hdrstop
+#include <ole2.h>
+USEFORM("TMain_form.cpp", Main_form);
+USEFORM("TFilespec_form.cpp", Filespec_import_form);
+USEFORM("TBatch_import_form.cpp", Batch_import_form);
+USELIB("general.lib");
+USELIB("C:\Program Files\Borland\CBuilder5\Lib\memmgr.lib");
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+   CoInitialize(NULL);
+
+   try
+   {
+      Application->Initialize();
+      Application->CreateHandle();  // DAH: 3/8/2000
+
+      Application->CreateForm(__classid(TMain_form), &Main_form);
+       Application->CreateForm(__classid(TFilespec_import_form), &Filespec_import_form);
+       Application->CreateForm(__classid(TBatch_import_form), &Batch_import_form);
+       Application->Run();
+   }
+   catch (Exception &exception)
+   {
+      Application->ShowException(&exception);
+   }
+   CoUninitialize();
+   return 0;
+}
+//---------------------------------------------------------------------------
