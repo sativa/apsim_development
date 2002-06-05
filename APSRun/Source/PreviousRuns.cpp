cb3a2ea33aa5ad1baa744990a8ffd9a1b974715b
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-06-05 02:23:11 +0000

    Upgraded to BCB6

--- APSRun/Source/PreviousRuns.cpp
+++ APSRun/Source/PreviousRuns.cpp
@@ -1,111 +1,52 @@
 //---------------------------------------------------------------------------
-
+#include <general\pch.h>
 #include <vcl.h>
 #pragma hdrstop
 
 #include "PreviousRuns.h"
-#include <aps\apsuite.h>
+#include <ApsimShared\ApsimSettings.h>
 using namespace std;
 
-static const char* PREVIOUS_RUN_SECTION = "most_recent_apsim_runs";
-static const char* NUM_REMEMBERED_RUNS = "num_apsim_runs_remembered";
-static const char* RUN_KEY = "run";
+static const char* NUM_REMEMBERED_RUNS = "most_recent_apsim_runs|num_apsim_runs_remembered";
+static const char* RUN_KEY = "most_recent_apsim_runs|run";
 static const char* CONFIGURATION_KEY = "configuration";
 static const char* SIMULATION_KEY = "simulation";
 
 // ------------------------------------------------------------------
-//  Short description:
-//     constructor
-
-//  Notes:
-
-//  Changes:
-//    DPH 11/7/2001
-
+// Constructor
 // ------------------------------------------------------------------
 PreviousRuns::PreviousRuns(void)
    {
    // get the maximum number of runs to remember in the .ini file.
    string st;
-   APSConfig().Read("apsuite", PREVIOUS_RUN_SECTION, NUM_REMEMBERED_RUNS, st);
+   ApsimSettings settings;
+   settings.read(NUM_REMEMBERED_RUNS, st);
    if (st == "")
       maxNumRememberedRuns = 10;
    else
       maxNumRememberedRuns = atoi(st.c_str());
 
    // get the current remembered runs.
-   readList(PREVIOUS_RUN_SECTION, RUN_KEY, rememberedRuns);
+   settings.read(RUN_KEY, rememberedRuns);
    }
 // ------------------------------------------------------------------
-//  Short description:
-//     read a list of key values from the .ini file for the
-//     specified application, section and key names.
-
-//  Notes:
-
-//  Changes:
-//    DPH 11/7/2001
-
-// ------------------------------------------------------------------
-void PreviousRuns::readList(const string& sectionName,
-                            const string& keyName,
-                            vector<string>& values)
-   {
-   string sts[MAX_NUM_KEYS];
-   int numSts;
-   APSConfig().Read_list("apsuite",
-                         sectionName.c_str(),
-                         keyName.c_str(),
-                         sts, numSts);
-   for (int i = 0; i < numSts; i++)
-      values.push_back(sts[i]);
-   }
-// ------------------------------------------------------------------
-//  Short description:
-//     write a list of key values to the .ini file for the
-//     specified application, section and key names.
-
-//  Notes:
-
-//  Changes:
-//    DPH 11/7/2001
-
-// ------------------------------------------------------------------
-void PreviousRuns::writeList(const string& sectionName,
-                             const string& keyName,
-                             vector<string>& values)
-   {
-   string sts[MAX_NUM_KEYS];
-   for (unsigned int i = 0; i < values.size(); i++)
-      sts[i] = values[i];
-   APSConfig().Write_list ("apsuite", sectionName.c_str(), keyName.c_str(),
-                           sts, values.size());
-   }
-// ------------------------------------------------------------------
-//  Short description:
-//     get the details of a specified run if possible.  Returns
-//     true if found.
-
-//  Notes:
-
-//  Changes:
-//    DPH 11/7/2001
-
+// Get the details of a specified run if possible.  Returns
+// true if found.
 // ------------------------------------------------------------------
 bool PreviousRuns::getPreviousRun(const string& controlFilename,
-                                 string& configurationName,
-                                 vector<string>&simulationNames)
+                                  string& configurationName,
+                                  vector<string>&simulationNames)
    {
    RememberedRuns::iterator i = find(rememberedRuns.begin(),
                                      rememberedRuns.end(),
                                      controlFilename);
    if (i != rememberedRuns.end())
       {
-      APSConfig().Read("apsuite", controlFilename.c_str(), CONFIGURATION_KEY,
-                       configurationName);
-
-      // get the current remembered runs.
-      readList(controlFilename.c_str(), SIMULATION_KEY, simulationNames);
+      ApsimSettings settings;
+      string configKey = controlFilename + "|" + CONFIGURATION_KEY;
+      string simulationKey = controlFilename + "|" + SIMULATION_KEY;
+      settings.read(configKey, configurationName);
+      settings.read(simulationKey, simulationNames);
       return true;
       }
    else
@@ -113,23 +54,19 @@ bool PreviousRuns::getPreviousRun(const string& controlFilename,
    }
 
 // ------------------------------------------------------------------
-//  Short description:
-//     set the most recent run to the one passed into this routine.
-
-//  Notes:
-
-//  Changes:
-//    DPH 11/7/2001
-
+// Set the most recent run to the one passed into this routine.
 // ------------------------------------------------------------------
 void PreviousRuns::setCurrentRun(const string& controlFilename,
                                  const string& configurationName,
                                  const vector<string>&simulationNames)
    {
+   ApsimSettings settings;
+
    // Write the control file section to the .ini file.
-   APSConfig().Write("apsuite", controlFilename.c_str(),
-                     CONFIGURATION_KEY, configurationName.c_str());
-   writeList(controlFilename.c_str(), string(SIMULATION_KEY), simulationNames);
+   string configKey = controlFilename + "|" + CONFIGURATION_KEY;
+   string simulationKey = controlFilename + "|" + SIMULATION_KEY;
+   settings.write(configKey, configurationName);
+   settings.write(simulationKey, simulationNames);
 
    // if this control file name isn't in the current list of
    // remembered runs, then add it to the front of the list.
@@ -147,7 +84,7 @@ void PreviousRuns::setCurrentRun(const string& controlFilename,
          i--;
          string controlFileToRemove = *i;
          rememberedRuns.erase(i);
-         APSConfig().Delete_section("apsuite", controlFileToRemove.c_str());
+         settings.erase(controlFileToRemove + "|");
          }
       }
    else
@@ -160,7 +97,7 @@ void PreviousRuns::setCurrentRun(const string& controlFilename,
    vector<string> newRuns;
    newRuns.push_back(controlFilename);
    copy(rememberedRuns.begin(), rememberedRuns.end(), back_inserter(newRuns));
-   writeList(PREVIOUS_RUN_SECTION, RUN_KEY, newRuns);
+   settings.write(RUN_KEY, newRuns);
    }
 
 
--- APSRun/Source/apsrun.cpp
+++ APSRun/Source/apsrun.cpp
@@ -1,130 +1,82 @@
 //---------------------------------------------------------------------------
+#include <general\pch.h>
 #include <vcl.h>
 #pragma hdrstop
 
-#include <aps\APSIMSimulationCollection.h>
-#include <aps\APSIMCONSimulationCollection.h>
-#include <general\string_functions.h>
-#include <general\stristr.h>
-#include <general\path.h>
-#include <fstream>
-#include <dos.h>
-#include <conio.h>
+#include <ApsimShared\ApsimControlFile.h>
+#include <ApsimShared\ApsimRunFile.h>
+#include <ApsimShared\ApsimSimulationFile.h>
 #include "TAPSIMRunForm.h"
-
-USERES("APSRun.res");
-USELIB("aps32.lib");
-USELIB("general.lib");
 USEFORM("TAPSIMRunForm.cpp", APSIMRunForm);
-USEUNIT("PreviousRuns.cpp");
 //---------------------------------------------------------------------------
 using namespace std;
-//---------------------------------------------------------------------------
+
+// ------------------------------------------------------------------
+// This application will be passed either a control file (.CON), a
+// run file (.RUN), or a .SIM file depending on what the user has
+// right clicked on.
+// ------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR,  int)
    {
-// ------------------------------------------------------------------
-//  Short description:
-//    This application will be passed either a control file (.CON), a
-//    run file (.RUN), or a .SIM file depending on what the user has
-//    right clicked on.
-
-//  Notes:
-
-//  Changes:
-//    DPH 28/10/97
-//    dph 7/7/2000 added support for .sim files.
-//    dph 17/1/2000 added support for /CREATESIM switch
-
-// ------------------------------------------------------------------
-   string filename;
-   bool quietRun = false;
-   bool createSIM = false;
-   for (int argIndex = 1; argIndex < _argc; argIndex++)
+   try
       {
-      if (stricmp(_argv[argIndex], "/q") == 0)
-         quietRun = true;
-      else if (stricmp(_argv[argIndex], "/CreateSIM") == 0)
-         createSIM = true;
-      else
-         filename = _argv[argIndex];
-      }
-   if (filename != "")
-      {
-      if (FileExists(filename.c_str()))
+      string fileName;
+      bool quietRun = false;
+      bool createSIM = false;
+      for (int argIndex = 1; argIndex < _argc; argIndex++)
          {
-         APSIMSimulationCollection* simulations = NULL;
+         if (stricmp(_argv[argIndex], "/q") == 0)
+            quietRun = true;
+         else if (stricmp(_argv[argIndex], "/CreateSIM") == 0)
+            createSIM = true;
+         else
+            fileName = _argv[argIndex];
+         }
+      if (!FileExists(fileName.c_str()))
+         throw runtime_error("Cannot locate APSIM file: " + fileName);
 
-         // Does the command line contain a control file?
-         if (stristr((char*) filename.c_str(), ".con") != NULL)
+      // Does the command line contain a control file?
+      if (ExtractFileExt(fileName.c_str()).AnsiCompareIC(".con") == 0)
+         {
+         // yes - better ask user for a configuration.
+         TAPSIMRunForm* runForm = new TAPSIMRunForm(NULL);
+         runForm->controlFilename = fileName;
+         if (runForm->ShowModal() == mrOk)
             {
-            simulations = new APSIMSimulationCollection;
-
-            // yes - better ask user for a configuration.
-            TAPSIMRunForm* runForm = new TAPSIMRunForm(NULL);
-            runForm->controlFilename = filename;
-            if (runForm->ShowModal() == mrOk)
-               runForm->getSelectedSimulations(*simulations);
+            vector<string> sections;
+            runForm->getSelectedSimulations(sections);
+            ApsimControlFile controlFile(fileName);
+            string configurationFile = runForm->getSelectedConfiguration();
             delete runForm;
-            }
-         else if (stristr((char*) filename.c_str(), ".run") != NULL)
-            {
-            simulations = new APSIMCONSimulationCollection(filename);
-            simulations->read();
-            }
 
-         else if (stristr((char*) filename.c_str(), ".sim") != NULL)
-            {
-            simulations = new APSIMSimulationCollection;
-            Path simPath(filename.c_str());
-            simulations->addSimulation(new APSIMSimulation
-               (simPath.Get_name_without_ext(), simPath.Get_path(), ""));
-            }
-
-         try
-            {
             if (createSIM)
                {
-               list<string> simulationNames;
-               simulations->getNames(simulationNames);
-               for (list<string>::iterator i = simulationNames.begin();
-                                           i != simulationNames.end();
-                                           i++)
-                  {
-                  simulations->get(*i)->readFromFile();
-                  simulations->get(*i)->writeToFile();
-                  }
+               for (vector<string>::iterator sectionI = sections.begin();
+                                             sectionI != sections.end();
+                                             sectionI++)
+                  controlFile.createSIM(*sectionI, configurationFile);
                }
             else
-               {
-               AllocConsole();
-               simulations->run();
-               if (!quietRun)
-                  MessageBox(NULL, "APSIM has finished", "For your information", MB_ICONINFORMATION | MB_OK);
-               FreeConsole();
-               }
+               controlFile.run(sections, configurationFile, quietRun);
             }
-         catch (string& msg)
-            {
-            MessageBox(NULL, msg.c_str(), "APSIM Error", MB_ICONSTOP | MB_OK);
-            }
-         delete simulations;
+         }
+      else if (ExtractFileExt(fileName.c_str()).AnsiCompareIC(".run") == 0)
+         {
+         ApsimRunFile run(fileName);
+         run.run(quietRun);
+         }
+      else if (ExtractFileExt(fileName.c_str()).AnsiCompareIC(".sim") == 0)
+         {
+         ApsimSimulationFile simulationFile(fileName);
+         simulationFile.run(quietRun);
          }
       else
-         {
-         string msg = "Cannot find file: " + filename;
-         MessageBox(NULL, msg.c_str(),
-                    "APSIM Error",
-                    MB_ICONSTOP | MB_OK);
-         }
-
+         throw runtime_error("Cannot run APSIM on file: " + fileName);
       }
-   else
+   catch (const runtime_error& error)
       {
-      MessageBox (NULL, "Usage:  APSRun [/Q] [/CreateSIM] [.con file | .run file | "
-                        ".sim file]",
-                  "Error",
-                  MB_ICONSTOP | MB_OK);
-
+      MessageBox(NULL, error.what(), "Error", MB_ICONSTOP | MB_OK);
+      return 1;
       }
    return 0;
    }
