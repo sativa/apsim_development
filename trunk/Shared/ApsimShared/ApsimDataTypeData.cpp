720dbde0f9c568d71c78814c0554f99e5947c9a5
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-06-21 02:04:08 +0000

    Added missing isStructure method

--- trunk/Shared/ApsimShared/ApsimDataTypeData.cpp
+++ trunk/Shared/ApsimShared/ApsimDataTypeData.cpp
@@ -23,63 +23,70 @@ unsigned ApsimDataTypeData::getNumFields(void) const
    return count;
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return true if data type is a structure.
+// ------------------------------------------------------------------
+bool ApsimDataTypeData::isStructure(void) const
+   {
+   return begin() != end();
+   }
+// ------------------------------------------------------------------
+// Return true if data type is an array.
 // ------------------------------------------------------------------
 bool ApsimDataTypeData::isArray(void) const
    {
    return (strcmpi(node.getAttribute("array").c_str(), "T") == 0);
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return true if data type is a built in type
 // ------------------------------------------------------------------
 bool ApsimDataTypeData::isBuiltIn(void) const
    {
    return (strcmpi(node.getAttribute("builtin").c_str(), "T") == 0);
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return true if data type is an array element
 // ------------------------------------------------------------------
 bool ApsimDataTypeData::isArrayElement(void) const
    {
    return (strcmpi(node.getName().c_str(), "element") == 0);
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return data type name
 // ------------------------------------------------------------------
 std::string ApsimDataTypeData::getName(void) const
    {
    return node.getAttribute("name");
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return data type kind
 // ------------------------------------------------------------------
 std::string ApsimDataTypeData::getKind(void) const
    {
    return node.getAttribute("kind");
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return data type description
 // ------------------------------------------------------------------
 std::string ApsimDataTypeData::getDescription(void) const
    {
    return node.getAttribute("description");
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return data type units
 // ------------------------------------------------------------------
 std::string ApsimDataTypeData::getUnit(void) const
    {
    return node.getAttribute("unit");
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return data type lower bound
 // ------------------------------------------------------------------
 std::string ApsimDataTypeData::getLowerBound(void) const
    {
    return node.getAttribute("lower_bound");
    }
 // ------------------------------------------------------------------
-// Return the number of fields this ApsimDataTypeData has.
+// Return data type upper bound.
 // ------------------------------------------------------------------
 std::string ApsimDataTypeData::getUpperBound(void) const
    {
