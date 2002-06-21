7ce84f041389a27bd5de8dfed7d9418b43e9e9b9
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-06-21 01:48:31 +0000

    Removed inline functions - causing an inline warning from compiler

--- trunk/Shared/ApsimShared/ApsimDataTypeData.h
+++ trunk/Shared/ApsimShared/ApsimDataTypeData.h
@@ -18,27 +18,16 @@ class __declspec(dllexport) ApsimDataTypeData
    public:
       ApsimDataTypeData(XMLNode n) : node(n) { }
 
-      bool isStructure(void) const
-         {return (begin() != end());}
-      bool isArray(void) const
-         {return (strcmpi(node.getAttribute("array").c_str(), "T") == 0);}
-      bool isBuiltIn(void) const
-         {return (strcmpi(node.getAttribute("builtin").c_str(), "T") == 0);}
-      bool isArrayElement(void) const
-         {return (strcmpi(node.getName().c_str(), "element") == 0);}
-      std::string getName(void) const
-         {return node.getAttribute("name");}
-
-      std::string getKind(void) const
-         {return node.getAttribute("kind");}
-      std::string getDescription(void) const
-         {return node.getAttribute("description");}
-      std::string getUnit(void) const
-         {return node.getAttribute("unit");}
-      std::string getLowerBound(void) const
-         {return node.getAttribute("lower_bound");}
-      std::string getUpperBound(void) const
-         {return node.getAttribute("upper_bound");}
+      bool isStructure(void) const;
+      bool isArray(void) const;
+      bool isBuiltIn(void) const;
+      bool isArrayElement(void) const;
+      std::string getName(void) const;
+      std::string getKind(void) const;
+      std::string getDescription(void) const;
+      std::string getUnit(void) const;
+      std::string getLowerBound(void) const;
+      std::string getUpperBound(void) const;
       unsigned getNumFields(void) const;
       typedef TreeNodeAliasIterator< TreeNodeIterator<XMLNode>, ApsimDataTypeData> iterator;
       iterator begin() const {return iterator(TreeNodeIterator<XMLNode>(node.begin()));}
