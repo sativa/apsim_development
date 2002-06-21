3d61cbc137b350e4b983315359a2c75ecbb28dc8
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-06-21 01:53:42 +0000

    Minor typo.

--- Shared/ApsimShared/ApsimRegistrationData.cpp
+++ Shared/ApsimShared/ApsimRegistrationData.cpp
@@ -8,28 +8,28 @@
 // ------------------------------------------------------------------
 // return true if this registration is an auto-register.
 // ------------------------------------------------------------------
-bool ApsimRegistration::doAutoRegister(void) const
+bool ApsimRegistrationData::doAutoRegister(void) const
    {
    return !Str_i_Eq(node.getAttribute("autoregister"), "F");
    }
 // ------------------------------------------------------------------
 // return the name of the registration.
 // ------------------------------------------------------------------
-std::string ApsimRegistration::getName(void) const
+std::string ApsimRegistrationData::getName(void) const
    {
    return node.getAttribute("name");
    }
 // ------------------------------------------------------------------
 // return the type of the registration.
 // ------------------------------------------------------------------
-std::string ApsimRegistration::getType(void) const
+std::string ApsimRegistrationData::getType(void) const
    {
    return node.getName();
    }
 // ------------------------------------------------------------------
 // return data type name of the registration.
 // ------------------------------------------------------------------
-std::string ApsimRegistration::getDataTypeName(void) const
+std::string ApsimRegistrationData::getDataTypeName(void) const
    {
    return node.getAttribute("type");
    }
