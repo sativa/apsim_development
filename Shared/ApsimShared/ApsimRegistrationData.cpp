6f1dfbab035ee25d93912d77080e96e502991145
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-06-24 06:00:58 +0000

    Added isOfType method

--- Shared/ApsimShared/ApsimRegistrationData.cpp
+++ Shared/ApsimShared/ApsimRegistrationData.cpp
@@ -33,4 +33,36 @@ std::string ApsimRegistrationData::getDataTypeName(void) const
    {
    return node.getAttribute("type");
    }
+// ------------------------------------------------------------------
+// return data type name of the registration.
+// ------------------------------------------------------------------
+bool ApsimRegistrationData::isOfType(const std::string& type) const
+   {
+   if (Str_i_Eq(type, "getVariable"))
+      return Str_i_Eq(node.getName(), "getVariableReg");
+   else if (Str_i_Eq(type, "setVariable"))
+      return Str_i_Eq(node.getName(), "setVariableReg");
+   else if (Str_i_Eq(type, "methodCall"))
+      return Str_i_Eq(node.getName(), "methodCallReg");
+   else if (Str_i_Eq(type, "event"))
+      return Str_i_Eq(node.getName(), "eventReg");
+   else if (Str_i_Eq(type, "respondToGet"))
+      return Str_i_Eq(node.getName(), "respondToGetReg") ||
+             Str_i_Eq(node.getName(), "respondToGetSetReg");
+   else if (Str_i_Eq(type, "respondToSet"))
+      return Str_i_Eq(node.getName(), "respondToSetReg") ||
+             Str_i_Eq(node.getName(), "respondToGetSetReg");
+   else if (Str_i_Eq(type, "respondToMethodCall"))
+      return Str_i_Eq(node.getName(), "respondToMethodCallReg");
+   else if (Str_i_Eq(type, "respondToEvent"))
+      return Str_i_Eq(node.getName(), "respondToEventReg");
+   else if (Str_i_Eq(type, "read"))
+      return Str_i_Eq(node.getName(), "read");
+   else
+      {
+      std::string msg = "Bad type of registration.  Type = ";
+      msg += type;
+      throw msg;
+      }
+   }
 
