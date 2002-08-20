0f7ffb6b058d250017bc4bf8eda17ee621f994b8
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-08-20 06:38:12 +0000

    no message

---
+++ apsim/Tracker/Source/TrackerComponent.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+#ifndef TrackerComponentH
+#define TrackerComponentH
+#include <ComponentInterface\Component.h>
+#include <string>
+#include <vector>
+#include <map>
+#include "TrackerVariable.h"
+// ------------------------------------------------------------------
+// TRACKER component for APSIM.
+// eg of parameter file specification:
+//    sum(rain)[1jan-31dec]
+//    sum(rain)[sow-harvest]
+//    sum(rain)[3]
+// ------------------------------------------------------------------
+class TrackerComponent : public protocol::Component
+   {
+   public:
+      TrackerComponent(void);
+      ~TrackerComponent(void);
+      virtual void doInit1(const FString& sdml);
+      virtual void doInit2(void);
+      virtual void respondToGet(unsigned int& fromID, protocol::QueryValueData& queryData);
+      virtual void respondToEvent(unsigned int& fromID, unsigned int& eventID, protocol::Variant& variant);
+
+   private:
+      std::vector<TrackerVariable> variables;
+
+   };
+
+
+#endif
