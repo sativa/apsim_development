fc3dd9ffa03d62c531928c3ccec6b46406fd0487
hug132 <hug132@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-08-21 04:15:20 +0000

    DamEasy pre-release version

--- trunk/ApsimOutlook/Source/Outlook/TDrill_down_form.h
+++ trunk/ApsimOutlook/Source/Outlook/TDrill_down_form.h
@@ -15,6 +15,7 @@
 #include <ExtCtrls.hpp>
 #include <ImgList.hpp>
 #include <Menus.hpp>
+#include "paramtreeview.hpp"
 #include <map>
 #include "Scenarios.h"
 
@@ -22,63 +23,54 @@
 class TDrill_down_form : public TForm
 {
 __published:	// IDE-managed Components
-   TTabControl *Tab_control;
    TImageList *ImageList;
    TPanel *Panel1;
    TBitBtn *Ok_button;
    TBitBtn *Cancel_button;
-   TScrollBox *ScrollBox;
-   TSpeedButton *SpeedButton1;
-   TSpeedButton *SpeedButton2;
-   TSpeedButton *SpeedButton3;
-   TSpeedButton *SpeedButton4;
-   TSpeedButton *SpeedButton5;
-   TSpeedButton *SpeedButton6;
-   TSpeedButton *SpeedButton7;
-   TSpeedButton *SpeedButton8;
-   TSpeedButton *SpeedButton9;
-   TSpeedButton *SpeedButton10;
-   TSpeedButton *SpeedButton11;
-   TSpeedButton *SpeedButton12;
-   TSpeedButton *SpeedButton13;
-   TSpeedButton *SpeedButton14;
-   TSpeedButton *SpeedButton15;
-   TSpeedButton *SpeedButton16;
-   TSpeedButton *SpeedButton17;
-   TSpeedButton *SpeedButton18;
-   TSpeedButton *SpeedButton19;
-   TSpeedButton *SpeedButton20;
-   TBitBtn *ClearButton;
-   TPopupMenu *TabPopup;
+   TPopupMenu *ScenarioNamePopup;
    TMenuItem *Rename1;
    TMenuItem *Delete1;
-   TPopupMenu *EmptyPopup;
+   TParamTreeview *ScenarioTree;
+   TPanel *Panel2;
+   TButton *ShowAllButton;
+   TButton *HideAllButton;
+   TBitBtn *ClearButton;
    void __fastcall FormShow(TObject *Sender);
-   void __fastcall Tab_controlChange(TObject *Sender);
+//   void __fastcall Tab_controlChange(TObject *Sender);
    void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
-   void __fastcall ButtonClick(TObject *Sender);
-   void __fastcall FormResize(TObject *Sender);
+//   void __fastcall ButtonClick(TObject *Sender);
    void __fastcall ClearButtonClick(TObject *Sender);
-   void __fastcall Rename1Click(TObject *Sender);
-   void __fastcall Delete1Click(TObject *Sender);
-   void __fastcall Tab_controlMouseDown(TObject *Sender,
+   void __fastcall ScenarioTreeParamClick(TObject *Sender,
+          TTreeNode *ANode, AnsiString href, AnsiString &value);
+   void __fastcall ScenarioTreeMouseDown(TObject *Sender,
           TMouseButton Button, TShiftState Shift, int X, int Y);
+   void __fastcall Rename1Click(TObject *Sender);
+   void __fastcall ShowAllButtonClick(TObject *Sender);
+   void __fastcall HideAllButtonClick(TObject *Sender);
+   void __fastcall Delete1Click(TObject *Sender);
+//   void __fastcall Rename1Click(TObject *Sender);
+//   void __fastcall Delete1Click(TObject *Sender);
+//   void __fastcall Tab_controlMouseDown(TObject *Sender,
+//          TMouseButton Button, TShiftState Shift, int X, int Y);
 private:	// User declarations
 
    bool Is_variable_attribute (const char* name);
 
    void Refresh (void);
    void RefreshScrollBox(void);
-   void Create_tabs (void);
+   void refreshScenarioTree (void);
    std::string Get_identifier_from_cell (int Col, int Row);
    std::string Get_value_from_cell (int Col, int Row);
+   void __fastcall popupClose(System::TObject* Sender, TCloseAction &Action);
+   int mouseDownX, mouseDownY;
+
 
 public:		// User declarations
    __fastcall TDrill_down_form(TComponent* Owner);
 
    Scenarios* scenarios;
 
-   void SetPresentationFonts(bool LargeFonts);
+//   void SetPresentationFonts(bool LargeFonts);
 };
 //---------------------------------------------------------------------------
 extern TDrill_down_form *Drill_down_form;
