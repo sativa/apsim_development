e61033ce084b5ba64f26b1ba80f851d31ded0409
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-08-28 05:26:37 +0000

    Added writeSection, deleteSection + removed the concept of a working .ini file.

--- trunk/Shared/ApsimShared/ApsimSettings.h
+++ trunk/Shared/ApsimShared/ApsimSettings.h
@@ -21,10 +21,10 @@ class __declspec(dllexport) ApsimSettings
       // Read in a setting for the specified key.  Key should be in the format:
       //    section1|section2|...|keyvalue.
       // Will throw if key value cannot be converted to int or double.
-      void read(const std::string& key, std::string& value);
-      void read(const std::string& key, int& value) throw(boost::bad_lexical_cast);
-      void read(const std::string& key, double& value) throw(boost::bad_lexical_cast);
-      void read(const std::string& key, std::vector<std::string>& values);
+      void read(const std::string& key, std::string& value) const;
+      void read(const std::string& key, int& value) const throw(boost::bad_lexical_cast);
+      void read(const std::string& key, double& value) const throw(boost::bad_lexical_cast);
+      void read(const std::string& key, std::vector<std::string>& values) const;
 
       // Write a setting for the specified key.
       void write(const std::string& key, const std::string& value);
@@ -34,13 +34,16 @@ class __declspec(dllexport) ApsimSettings
 
       // Return a complete list of all keys under the specified key.
       void getKeysUnder(const std::string& key, std::vector<std::string>& keys);
+      void getSectionNames(vector<string>& sections) const;
 
       // Erase the specified key.  If key is a section then all child keys
       // will also be removed.
       void erase(const std::string& key);
 
-      // Read in the contents of a given section.
-      void readSection(const std::string& sectionName, std::string& contents);
+      // Read and write section contents.
+      void readSection(const std::string& sectionName, std::string& contents) const;
+      void writeSection(const std::string& sectionName, std::string& contents) const;
+      void deleteSection(const std::string& sectionName) const;
 
       // Return the folder where all APSIM settings are located.
       // Will throw if the current application is not in the apsim directory.
@@ -48,10 +51,10 @@ class __declspec(dllexport) ApsimSettings
 
    private:
       IniFile* original;
-      IniFile* working;
+//      IniFile* working;
 
-      std::string getSection(const std::string& key);
-      std::string getKey(const std::string& key);
+      std::string getSection(const std::string& key) const;
+      std::string getKey(const std::string& key) const;
    };
 
 #endif
