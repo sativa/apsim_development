f8bef88983079ad52d14b8f45af0c3f65b59a430
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-09-11 02:46:01 +0000

    Upgraded to bcb6

--- ApsimOutlook/Source/Manager/TFilespec_form.h
+++ ApsimOutlook/Source/Manager/TFilespec_form.h
@@ -6,19 +6,21 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
-#include "ToolEdit.hpp"
 #include <Buttons.hpp>
 #include <Mask.hpp>
+#include "AdvDirectoryEdit.hpp"
+#include "AdvEdBtn.hpp"
+#include "AdvEdit.hpp"
 //---------------------------------------------------------------------------
 class TFilespec_import_form : public TForm
 {
 __published:	// IDE-managed Components
-   TDirectoryEdit *DirectoryEdit;
    TLabel *Label1;
    TLabel *Label2;
    TEdit *FilespecEdit;
    TBitBtn *BitBtn1;
    TBitBtn *BitBtn2;
+   TAdvDirectoryEdit *DirectoryEdit;
 private:	// User declarations
 public:		// User declarations
    __fastcall TFilespec_import_form(TComponent* Owner);
