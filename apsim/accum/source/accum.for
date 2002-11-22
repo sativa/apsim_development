b48688d7212228b73ed383b42a811469c914a6fa
har297 <har297@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-11-22 03:02:32 +0000

    no message

--- apsim/accum/source/accum.for
+++ apsim/accum/source/accum.for
@@ -1,263 +1,159 @@
-      include 'accum.inc'
-
+       include 'Accum.inc'
+       
 !     ===========================================================
-      subroutine alloc_dealloc_instance(doAllocate)
+      subroutine AllocInstance (InstanceName, InstanceNo)
 !     ===========================================================
       use AccumModule
       implicit none
-      ml_external alloc_dealloc_instance
-
+ 
 !+  Sub-Program Arguments
-      logical, intent(in) :: doAllocate
-
+      character InstanceName*(*)       ! (INPUT) name of instance
+      integer   InstanceNo             ! (INPUT) instance number to allocate
+ 
 !+  Purpose
 !      Module instantiation routine.
-
+ 
 !- Implementation Section ----------------------------------
-
-      if (doAllocate) then
-         allocate(id)
-         allocate(g)
-
-      else
-         deallocate(id)
-         deallocate(g)
-
-      end if
+               
+      allocate (Instances(InstanceNo)%gptr)
+      Instances(InstanceNo)%Name = InstanceName
+ 
       return
       end
 
 !     ===========================================================
-      subroutine do_init1(sdml)
+      subroutine FreeInstance (anInstanceNo)
 !     ===========================================================
       use AccumModule
-      use DataTypesModule
       implicit none
-      ml_external do_init1
-
-!+  Purpose
-!      Perform all registrations and zeroing
-
+ 
 !+  Sub-Program Arguments
-      character (len=*), intent(in) :: sdml
-
-*+  Constant Values
-       character ID_Init*(*)          ! Message indicating initialisation
-       parameter (ID_Init='Initialising')
-
-*+  Local Variables
-       character Event_string*40       ! String to output
-
-*- Implementation Section ----------------------------------
-
-      call do_registrations(id)
-
-      return
-      end
-
-!     ===========================================================
-      subroutine do_commence()
-!     ===========================================================
-      implicit none
-      ml_external do_commence
-
+      integer anInstanceNo             ! (INPUT) instance number to allocate
+ 
 !+  Purpose
-!      Perform all registrations and zeroing
-
+!      Module de-instantiation routine.
+ 
 !- Implementation Section ----------------------------------
-
+               
+      deallocate (Instances(anInstanceNo)%gptr)
+ 
       return
       end
-
-* ====================================================================
-      subroutine do_init2 ()
-* ====================================================================
-      use AccumModule
-      use ComponentInterfaceModule
-      implicit none
-      ml_external do_init2
-
-*+  Purpose
-*     Initialise the apswim module
-
-*+  Changes
-
-*+  Calls
-
-*+  Constant Values
-       character ID_Init*(*)          ! Message indicating initialisation
-       parameter (ID_Init='Initialising')
-
-*+  Local Variables
-       character Event_string*40       ! String to output
-
-*- Implementation Section ----------------------------------
-
-      ! Notify system that we have initialised
-
-      Event_string = ID_Init
-      call Write_string (Event_string)
-
-      ! Get all parameters from parameter file
-
-      call Accum_read_param ()
-
-      return
-      end
-
-
+     
 !     ===========================================================
-      subroutine respondToEvent(fromID, eventID, variant)
+      subroutine SwapInstance (anInstanceNo)
 !     ===========================================================
       use AccumModule
-      use ComponentInterfaceModule
-      use DataTypesModule
       implicit none
-      ml_external respondToEvent
-
-!+  Purpose
-!      Event handler for all events coming into module.
-
+ 
 !+  Sub-Program Arguments
-      integer, intent(in) :: fromID
-      integer, intent(in) :: eventID
-      integer, intent(in) :: variant
-
-!- Implementation Section ----------------------------------
-
-      if (eventID .eq. id%post) then
-         call Accum_get_other_variables()
-
-      else
-         call error('bad event ID',.true.)
-      endif
-      return
-      end
-!     ===========================================================
-      subroutine respondToMethod(fromID, methodID, variant)
-!     ===========================================================
-      use AccumModule
-      use ComponentInterfaceModule
-      implicit none
-      ml_external respondToMethod
-
+      integer anInstanceNo             ! (INPUT) instance number to allocate
+ 
 !+  Purpose
-!      Method handler for all method calls coming into module.
-
-!+  Sub-Program Arguments
-      integer, intent(in) :: fromID
-      integer, intent(in) :: methodID
-      integer, intent(in) :: variant
-
+!      Swap an instance into the global 'g' pointer
+ 
 !- Implementation Section ----------------------------------
-
-      call error('bad method ID',.true.)
-
+               
+      g => Instances(anInstanceNo)%gptr
+ 
       return
       end
-
-!     ===========================================================
-      subroutine notify_termination()
-!     ===========================================================
-      use AccumModule
-      implicit none
-      ml_external notify_termination
-
-!+  Purpose
-!      Prepare for termination
-
-!- Implementation Section ----------------------------------
-
-      return
-      end
-
+       
 * ====================================================================
-       subroutine respondToGet (fromID, Variable_info)
+       subroutine Main (Action, Data)
 * ====================================================================
       use AccumModule
-      use ComponentInterfaceModule
-      use DataTypesModule
       implicit none
-      ml_external respondToGet
+       include 'const.inc'             ! Global constant definitions
+       include 'action.inc'
 
 *+  Sub-Program Arguments
-      integer, intent(in) :: fromID
-      type(QueryData), intent(in) :: variable_info
-
+       character Action*(*)            ! Message action to perform
+       character Data*(*)              ! Message data
+*
 *+  Purpose
-*      Return the value of one of our variables to caller
+*      This routine is the interface between the main system and the
+*      Accum module.
 
 *+  Changes
+*     DPH 26/10/95  Added call to message_unused
+*     jngh 09/06/96 added version to presence report
+*     dph 7/5/99 removed presence if test. c186
+
+*- Implementation Section ----------------------------------
+ 
+      if (Action.eq.ACTION_Init) then
+         call Accum_zero_variables ()
+         call Accum_Init ()
+ 
+      else if (Action .eq. ACTION_Post) then
+         call Accum_get_other_variables()
+ 
+      else if (Action.eq.ACTION_Get_variable) then
+         ! respond to request for one of our variable values
+ 
+         call Accum_send_my_variable (Data)
+ 
+      else
+         ! Don't use message
+ 
+         call Message_unused ()
+      endif
+ 
+      return
+      end
+
+
+
+* ====================================================================
+       subroutine Accum_Init ()
+* ====================================================================
+      use AccumModule
+      implicit none
+       include 'const.inc'             ! Constant definitions
+
+*+  Purpose
+*      Initialise Accum module
+
+*+  Changes
+*     ????
+*     jngh removed a_ from version function
+*     dph 7/5/99 removed version info. c186
 
 *+  Calls
 
 *+  Constant Values
-      character  myname*(*)            ! name of this procedure
-      parameter (myname = 'respondToGet')
+       character ID_Init*(*)          ! Message indicating initialisation
+       parameter (ID_Init='Initialising')
 
 *+  Local Variables
-       integer variable
-       integer day
-       real sum
+       character Event_string*40       ! String to output
 
 *- Implementation Section ----------------------------------
-
-      call push_routine (myname)
-
-      do variable = 1, g%num_variables
-         ! If variable is ours then accumulate the variable for the
-         ! specified number of days and return the value to the caller.
-         if (Variable_info%id .eq.
-     .       g%Variable_respondtogetids(variable)) then
-            Sum = 0.0
-            do day = 1, g%Variable_sizes(variable)
-               Sum = Sum + g%variable_values(variable, day)
-            end do
-
-            call return_single(Variable_info, Sum)
-         endif
-      end do
-
-      call pop_routine(myname)
-
+ 
+      ! Notify system that we have initialised
+ 
+      Event_string = ID_Init
+      call Write_string (Event_string)
+ 
+      ! Get all parameters from parameter file
+ 
+      call Accum_read_param ()
+ 
       return
       end
-* ====================================================================
-      logical function respondToSet (fromID, VariableID, variant)
-* ====================================================================
-      use AccumModule
-      use ComponentInterfaceModule
-
-      implicit none
-      ml_external respondToSet
-
-!+  Sub-Program Arguments
-      integer, intent(in) :: fromID
-      integer, intent(in)     :: VariableID
-      integer, intent(in out) :: variant
 
 
-*+  Purpose
-*       Set one of our variables altered by some other module.
-
-*+  Changes
-*      21-06-96 NIH Changed respond2set calls to collect calls
-
-*+  Local Variables
-
-*- Implementation Section ----------------------------------
-
-      respondToSet = .false.
-      return
-      end
 
 * ====================================================================
        subroutine Accum_read_param ()
 * ====================================================================
       use AccumModule
-      use ComponentInterfaceModule
-      use DataTypesModule
       implicit none
+      include 'const.inc'              ! Constant definitions
+      include 'datastr.pub'                       
+      include 'read.pub'                          
+      include 'error.pub'                         
 
 *+  Purpose
 *      Read in all parameters from parameter file.
@@ -291,58 +187,46 @@
        integer Numvals                 ! Number of values
        integer Pos                     ! Position in string
        character Size_string*50        ! string version of size
-      logical found
 
 *- Implementation Section ----------------------------------
-
+ 
       call push_routine(This_routine)
-
-      found = read_parameter(section_name, 'accum_variables',
-     .   g%variable_names, g%num_variables)
-
+ 
+      call read_char_array(section_name, 'accum_variables',
+     .   Max_variables, ' ', g%variable_names, g%num_variables)
+ 
       ! loop through each variable and pull off size component.
-
+ 
       do 10 indx = 1, g%num_variables
          Pos = index(g%variable_names(indx), '[')
          if (Pos .eq. 0) then
-            call error(Err2, .true.)
-
+            call Fatal_error(Err_internal, Err2)
+ 
          else
-            ! Register the variable as a respondToGet
-            g%Variable_respondtogetids(indx) = add_registration
-     .            (respondToGetReg, g%variable_names(indx),
-     .             singleddml)
-
-
             ! Extract size component.
-
+ 
             Size_string = g%variable_names(indx)(Pos + 1:)
             g%variable_names(indx)(Pos:) = Blank
             Pos = index(Size_string, ']')
             if (Pos .eq. 0) then
-               call error(Err2, .true.)
-
+               call Fatal_error(Err_internal, Err2)
+ 
             else
                Size_string(Pos:) = Blank
                call String_to_integer_var(Size_string,
      .              g%variable_sizes(indx), Numvals)
-
-               ! Also register that we are going to get the value of the raw variable
-               g%Variable_getids(indx) = add_registration
-     .               (getVariableReg, g%variable_names(indx),
-     .                singleddml)
-
+ 
                if (g%variable_sizes(indx) .gt. 0 .and.
      .             g%variable_sizes(g%num_variables) .le. Max_days) then
                   goto 10
-
+ 
                else
-                  call error(Err3, .true.)
+                  call Fatal_error(Err_internal, Err3)
                endif
             endif
          endif
 10    continue
-
+ 
       call pop_routine(This_routine)
       return
       end
@@ -366,13 +250,13 @@
       integer Day_index                ! day index
 
 *- Implementation Section ----------------------------------
-
+ 
       g%num_variables = 0
       do 10 Var_index = 1, Max_variables
          do 10 Day_index = 1, Max_days
             g%variable_values(Var_index, Day_index) = 0.0
 10    continue
-
+ 
       return
       end
 
@@ -382,9 +266,9 @@
        subroutine Accum_get_other_variables ()
 * ====================================================================
       use AccumModule
-      use ComponentInterfaceModule
-      use DataTypesModule
       implicit none
+       include 'const.inc'             ! Constant definitions
+      include 'intrface.pub'                      
 
 *+  Purpose
 *      Get the values of variables from other modules
@@ -396,25 +280,123 @@
       integer Day_index                ! index into day part of variable_sizes array
       integer Var_index                ! Index into variable arrays
       integer Numvals                  ! Number of values returned
-      logical ok
 
 *- Implementation Section ----------------------------------
-
+ 
       ! Move all other variables down one position in the array.
-
+ 
       do 10 Var_index = 1, g%num_variables
          do 10 Day_index =  g%variable_sizes(Var_index), 2, -1
             g%variable_values(Var_index, Day_index) =
      .         g%variable_values(Var_index, Day_index - 1)
 10    continue
-
+ 
       ! Get all required variables for today
-
+ 
       do 20 Var_index = 1, g%num_variables
-         ok = Get_single(g%variable_getids(Var_index),
-     .        g%variable_values(Var_index, 1))
+         call Get_real_var(Unknown_module, g%variable_names(Var_index),
+     .        ' ', g%variable_values(Var_index, 1), Numvals, -10000.0,
+     .        10000.0)
 20    continue
-
+ 
       return
       end
+
+
+
+* ====================================================================
+       subroutine Accum_Send_my_variable (Variable_name)
+* ====================================================================
+      use AccumModule
+      implicit none
+       include 'const.inc'             ! constant definitions
+      include 'intrface.pub'                      
+      include 'datastr.pub'                       
+      include 'error.pub'                         
+
+*+  Sub-Program Arguments
+       character Variable_name*(*)     ! (INPUT) Variable name to search for
+
+*+  Purpose
+*      Return the value of one of our variables to caller
+
+*+  Changes
+*     DPH 26/10/95 Added call to message_unused
+*     DPH 30/5/96  Moved the first endif past the Find_string_in_array call
+*                  so that we only look for our variables that have a '[' char.
+
+*+  Constant Values
+      character Routine_name*(*)       ! Name of this routine
+      parameter (Routine_name='accum_send_my_variable')
+
+*+  Local Variables
+      integer Day_index                ! Index into day part of g%variable_values array
+      logical Found                    ! Have we found the variable yet ?
+      integer Num_days                 ! Number of days to accumulate.
+      integer Numvals                  ! number of values
+      integer Pos                      ! Position in string
+      character Size_string*100        ! String version of size
+      real Sum                         ! Accumulated value
+      integer Var_index                ! Index into variable arrays
+      character Var_name*50            ! Variable name requested.
+
+*- Implementation Section ----------------------------------
+ 
+      call push_routine(Routine_name)
+ 
+      Pos = index(Variable_name, '[')
+ 
+      if (Pos .eq. 0) then
+         ! None found
+ 
+         Found = .false.
+ 
+      else
+         Var_name = Variable_name(1:Pos-1)
+         Size_string = Variable_name(Pos + 1:)
+         Pos = index(Size_string, ']')
+         if (Pos .gt. 0) then
+            Size_string(Pos:) = Blank
+            call String_to_integer_var(Size_string, Num_days, Numvals)
+         else
+            Numvals = 0
+         endif
+ 
+         ! Determine weather the requested variable is one of ours.
+ 
+         Var_index = Find_string_in_array
+     .      (Var_name, g%variable_names, g%num_variables)
+         Found = (Var_index .gt. 0)
+      endif
+ 
+      ! If variable is ours then accumulate the variable for the
+      ! specified number of days and return the value to the caller.
+ 
+      if (Found) then
+ 
+         if (Num_days .eq. -1) then
+            Num_days = g%variable_sizes(Var_index)
+ 
+         else
+            ! Caller has specified the number of days.
+         endif
+ 
+         Sum = 0.0
+         do 20 Day_index = 1, Num_days
+            Sum = Sum + g%variable_values(Var_index, Day_index)
+20       continue
+ 
+         call respond2get_real_var(Variable_name, '()', Sum)
+ 
+      else
+         ! Not our variable
+ 
+         call Message_unused ()
+      endif
+ 
+      call pop_routine(Routine_name)
+      return
+      end
+
+
 
