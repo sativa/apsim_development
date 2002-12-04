daa8ef32cf6eb3791ca8bdf4885da41e21f1a95f
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-12-04 02:29:14 +0000

    Module now a F90 module

--- trunk/apsim/SOI/source/soi.for
+++ trunk/apsim/SOI/source/soi.for
@@ -1,68 +1,109 @@
+      module SOIModule
+! ========================================================================
+!     SOI constants
+! ========================================================================
+
+!   Short description:
+!      General variables used in SOI model
+
+!   Assumptions:
+!      None
+
+!   Notes:
+
+!   Changes:
+
+! ====================Declaration section=================================
+
+!   Global variables
+      integer   SOI_min                          ! Start year of SOI array
+      parameter (SOI_min = 1800)
+      integer   SOI_max                          ! End year of SOI array
+      parameter (SOI_max = 2100)
+
+      type SOIGlobals
+         integer   LU_SOI                        ! Unit number for SOI file
+         integer   SOI_phase
+         integer   SOI_array(SOI_min:SOI_max,12) ! SOI phases array
+         double precision SOI_jday               ! System jday
+      end type SOIGlobals
+
+! ========================================================================
+      ! instance variables.
+      type (SOIGlobals), pointer :: g
+      integer MAX_NUM_INSTANCES
+      parameter (MAX_NUM_INSTANCES=10)
+      integer MAX_INSTANCE_NAME_SIZE
+      parameter (MAX_INSTANCE_NAME_SIZE=50)
+      type SOIDataPtr
+         type (SOIGlobals), pointer ::    gptr
+         character Name*(MAX_INSTANCE_NAME_SIZE)
+      end type SOIDataPtr
+      type (SOIDataPtr), dimension(MAX_NUM_INSTANCES) :: Instances
+
+      contains
+
 !     ===========================================================
       subroutine AllocInstance (InstanceName, InstanceNo)
 !     ===========================================================
-      use SOIModule
       Use infrastructure
       implicit none
- 
+
 !+  Sub-Program Arguments
       character InstanceName*(*)       ! (INPUT) name of instance
       integer   InstanceNo             ! (INPUT) instance number to allocate
- 
+
 !+  Purpose
 !      Module instantiation routine.
- 
+
 !- Implementation Section ----------------------------------
-               
+
       allocate (Instances(InstanceNo)%gptr)
       Instances(InstanceNo)%Name = InstanceName
- 
+
       return
-      end
+      end subroutine
 
 !     ===========================================================
       subroutine FreeInstance (anInstanceNo)
 !     ===========================================================
-      use SOIModule
       Use infrastructure
       implicit none
- 
+
 !+  Sub-Program Arguments
       integer anInstanceNo             ! (INPUT) instance number to allocate
- 
+
 !+  Purpose
 !      Module de-instantiation routine.
- 
+
 !- Implementation Section ----------------------------------
-               
+
       deallocate (Instances(anInstanceNo)%gptr)
-       
+
       return
-      end
-     
+      end subroutine
+
 !     ===========================================================
       subroutine SwapInstance (anInstanceNo)
 !     ===========================================================
-      use SOIModule
       Use infrastructure
       implicit none
- 
+
 !+  Sub-Program Arguments
       integer anInstanceNo             ! (INPUT) instance number to allocate
- 
+
 !+  Purpose
 !      Swap an instance into the global 'g' pointer
- 
+
 !- Implementation Section ----------------------------------
-               
+
       g => Instances(anInstanceNo)%gptr
-       
+
       return
-      end
+      end subroutine
 * ====================================================================
       recursive subroutine Main (action, data)
 * ====================================================================
-      use SOIModule
       Use infrastructure
       implicit none
 
@@ -81,34 +122,33 @@
 *+  Constant Values
 
 *- Implementation Section ----------------------------------
- 
+
       if (action .eq. ACTION_init) then
- 
+
          ! initialise variables for run (called once only)
- 
+
          call SOI_init ()
- 
+
       else if (action .eq. ACTION_get_variable) then
- 
+
          ! return one of our variables to calling module
- 
+
       call SOI_send_my_variable (data)
- 
+
       else
          ! don't use message
- 
+
          call Message_unused ()
       endif
- 
+
       return
-      end
+      end subroutine
 
 
 
 *     ===========================================================
       subroutine SOI_init ()
 *     ===========================================================
-      use SOIModule
       Use infrastructure
       implicit none
 
@@ -130,9 +170,9 @@
       logical ok
 
 *- Implementation Section ----------------------------------
- 
+
       call push_routine (this_routine)
- 
+
       ! Zero variables
       call SOI_zero_variables()
 
@@ -149,14 +189,13 @@
 
       call pop_routine (this_routine)
       return
-      end
+      end subroutine
 
 
 
 *     ===========================================================
       subroutine SOI_zero_variables ()
 *     ===========================================================
-      use SOIModule
       Use infrastructure
       implicit none
 
@@ -175,32 +214,31 @@
         integer y                       ! do loop counter
 
 *- Implementation Section ----------------------------------
- 
+
       call push_routine (this_routine)
- 
+
       ! zero SOI array
- 
+
       do x = SOI_min, SOI_max
         do y = 1, 12
             g%SOI_array(x,y) = 0
         enddo
       enddo
- 
+
       g%SOI_phase = 0
       g%LU_SOI = 0
- 
- 
+
+
       call pop_routine (this_routine)
- 
+
       return
-      end
+      end subroutine
 
 
 
 * ====================================================================
       subroutine SOI_read_phases ()
 * ====================================================================
-      use SOIModule
       Use infrastructure
       implicit none
 
@@ -221,11 +259,11 @@
       integer numvals
 
 *- Implementation Section ----------------------------------
- 
+
       call push_routine (this_routine)
- 
+
       ! Loop through the SOI file, reding the phases into an array
- 
+
 10    continue
       ok = ExternalTable_GetValueByIndex (g%LU_SOI, 0, St)
       if (ok) then
@@ -241,34 +279,33 @@
       endif
       if (ok) then
          ! check if input values are ok
- 
+
          call bound_check_integer_var(Year,
      .                                SOI_min,
      .                                SOI_max,
      .                                'SOI_inp_year')
- 
+
          call bound_check_integer_var(Month,
      .                                1,
      .                                12,
      .                                'SOI_inp_month')
- 
+
          g%SOI_array(Year, Month) = Phase
          call ExternalTable_Next(g%LU_SOI)
          if (.not. ExternalTable_eof(g%LU_SOI)) then
             goto 10
          endif
       endif
- 
+
       call pop_routine (this_routine)
       return
-      end
+      end subroutine
 
 
 
 *     ================================================================
       recursive subroutine SOI_send_my_variable (variable_name)
 *     ================================================================
-      use SOIModule
       Use infrastructure
       implicit none
 
@@ -286,33 +323,32 @@
       parameter (this_routine='SOI_send_my_variable')
 
 *- Implementation Section ----------------------------------
- 
+
       call push_routine (this_routine)
- 
+
       if (variable_name(1:4) .eq. 'soi[') then
- 
+
          call SOI_get_phase (variable_name)
- 
+
          call respond2get_integer_var(variable_name, ! external name
      .                             '()',       ! units of var.
      .                             g%SOI_phase)     ! internal var. name
- 
+
       else
          call Message_unused ()
- 
+
       endif
- 
+
       call pop_routine (this_routine)
- 
+
       return
-      end
+      end subroutine
 
 
 
 *     ================================================================
       recursive subroutine SOI_get_phase (variable_name)
 *     ================================================================
-      use SOIModule
       Use infrastructure
       implicit none
 
@@ -339,11 +375,11 @@
       integer   SOI_get_units          ! function
 
 *- Implementation Section ----------------------------------
- 
+
       call push_routine (this_routine)
- 
+
       ! get date from system
- 
+
       call get_double_var (Unknown_module,
      .                      'today',
      .                      '()',
@@ -351,26 +387,26 @@
      .                      numvals,
      .                      0.0d0,
      .                      3660000000.0d0)
- 
+
 
       call jday_to_date (SOI_Day, SOI_Month, SOI_Year, g%SOI_jday)
- 
+
       ! get the month or lag from the variable name
- 
+
       SOI_units = SOI_get_units (variable_name)
 
       ! Check for a valid month. We only do 1 year ahead or behind
- 
+
       if (SOI_units .gt. 12) then
          SOI_units = 12
       endif
- 
+
       if (SOI_units .lt. -12) then
          SOI_units = -12
       endif
- 
+
       ! If the Units are less than one then treat it as a lag
- 
+
       if (SOI_units .lt. 1) then
           SOI_Month = SOI_Month + SOI_units
           if (SOI_Month .lt. 1) then
@@ -380,23 +416,22 @@
       else
           SOI_Month = SOI_units
       endif
- 
+
       ! get the SOI phase for the given month
- 
+
       g%SOI_phase = g%SOI_array(SOI_year, SOI_Month)
       ! print *, SOI_units,SOI_Month,SOI_year,g%SOI_phase
- 
+
       call pop_routine (this_routine)
- 
+
       return
-      end
+      end subroutine
 
 
 
 * =============================================================
       integer function SOI_get_units (record)
 * =============================================================
-      use SOIModule
       Use infrastructure
       implicit none
 
@@ -430,33 +465,34 @@
       character  values*100            ! values string
 
 *- Implementation Section ----------------------------------
- 
+
       call push_routine (this_routine)
- 
+
       ! Get the string between the square brackets
- 
+
       call split_line (record, values, unit_plus, unit_start)
       call split_line (unit_plus, units, remainder, unit_end)
- 
+
       SOI_get_units = 0
- 
+
       if (units .ne. ' ') then
- 
+
          ! Test to see if the string is a date in string format or not
- 
+
          read (units,*,iostat = SOI_error) SOI_get_units
          if (SOI_error .ne. 0) then
- 
+
             ! Convert the date sring to a month number
 
             SOI_jday = date(units, g%SOI_jday)
             call jday_to_date(SOI_day,SOI_get_units,SOI_year,SOI_jday)
          endif
       endif
- 
+
       call pop_routine (this_routine)
       return
-      end
+      end subroutine
 
 
 
+      end module SOIModule
