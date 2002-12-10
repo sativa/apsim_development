35527b3cfc7b7e7ff73c83946b0837bfcefc6d4c
har297 <har297@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2002-12-10 04:18:48 +0000

    Externalised Instance Allocation and subroutine 'Main' for APSIM2.2

--- trunk/apsim/sorghum/source/sorghum.for
+++ trunk/apsim/sorghum/source/sorghum.for
@@ -4,10 +4,42 @@
 
       contains
 
-      include 'cropmodmain.for'
+      include 'cropmodcomms.for'
       include 'sorgmain.for'
       include 'sorgn.for'
       include 'sorgopt.for'
       include 'sorgtree.for'
 
       end module
+
+
+!     ===========================================================
+      subroutine alloc_dealloc_instance(doAllocate)
+!     ===========================================================
+      use sorghummodule
+      implicit none
+      ml_external alloc_dealloc_instance
+
+!+  Sub-Program Arguments
+      logical, intent(in) :: doAllocate
+
+!+  Purpose
+!      Module instantiation routine.
+
+!- Implementation Section ----------------------------------
+
+      if (doAllocate) then
+         allocate(g)
+         allocate(p)
+         allocate(c)
+      else
+         deallocate(g)
+         deallocate(p)
+         deallocate(c)
+      end if
+      return
+      end subroutine
+
+
+      include 'CropModMain.for'
+      
