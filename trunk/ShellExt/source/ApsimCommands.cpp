ee7c8242c217388123fff8873d2d51f549a6c2db
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-02-25 00:07:15 +0000

    no message

---
+++ trunk/ShellExt/source/ApsimCommands.cpp
@@ -0,0 +1,168 @@
+//---------------------------------------------------------------------------
+#include <general\pch.h>
+#include <vcl.h>
+#pragma hdrstop
+
+#include "ApsimCommands.h"
+#include <general\string_functions.h>
+#include <general\path.h>
+#include <general\stream_functions.h>
+#include <apsimshared\apsimdirectories.h>
+#pragma package(smart_init)
+
+//---------------------------------------------------------------------------
+// Send all files to EXCEL.  Files is a CSV list of filenames.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall excelFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   for (unsigned int file = 0; file != fileNames.size(); file++)
+      {
+      // open input stream
+      ifstream in (fileNames[file].c_str());
+
+      // output output stream.
+      Path OutPath (fileNames[file]);
+      OutPath.Set_extension (".csv");
+      ofstream out (OutPath.Get_path().c_str());
+
+      // copy first two lines as is.
+      string Line;
+      getline (in, Line);
+      out << Line << endl;
+      getline (in, Line);
+      out << Line << endl;
+
+      // convert file.
+      Convert_2_CSV(in, out);
+
+      // close files.
+      in.close();
+      out.close();
+
+      // give output file to EXCEL.
+      ShellExecute (NULL, "open", OutPath.Get_path().c_str(), NULL, "", SW_SHOW);
+      }
+   }
+//---------------------------------------------------------------------------
+// Send all files to APSVis.  Files is a CSV list of filenames.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall apsvisFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   // write response file.
+   string responseFile = Path::getTempFolder().Get_path() + "\\response.file";
+   ofstream out(responseFile.c_str());
+   for (unsigned i = 0; i != fileNames.size(); ++i)
+      out << fileNames[i] << endl;
+   out.close();
+
+   // pass response file to apsimoutlook.
+   string command = "\"" + getApsimDirectory() + "\\bin\\apsvis\" " + responseFile;
+   WinExec(command.c_str(), SW_SHOW);
+   }
+//---------------------------------------------------------------------------
+// Send all files to Apsim Outlook.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall apsimoutlookFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   // write response file.
+   string responseFile = Path::getTempFolder().Get_path() + "\\response.file";
+   ofstream out(responseFile.c_str());
+   for (unsigned i = 0; i != fileNames.size(); ++i)
+      out << fileNames[i] << endl;
+   out.close();
+
+   // pass response file to apsimoutlook.
+   string command = "\"" + getApsimDirectory() + "\\bin\\apsimoutlook\" " + responseFile;
+   WinExec(command.c_str(), SW_SHOW);
+   }
+//---------------------------------------------------------------------------
+// Send all files to Apsim.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall runFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   for (unsigned int file = 0; file != fileNames.size(); file++)
+      {
+      string command = "\"" + getApsimDirectory() + "\\bin\\apsrun\" \"" + fileNames[file] + "\"";
+
+      // run command
+      WinExec(command.c_str(), SW_SHOW);
+      }
+   }
+//---------------------------------------------------------------------------
+// Convert all files to SIM format.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall createSimFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   for (unsigned int file = 0; file != fileNames.size(); file++)
+      {
+      string command = "\"" + getApsimDirectory() + "\\bin\\apsrun\" /CreateSIM \"" + fileNames[file] + "\"";
+
+      // run command
+      WinExec(command.c_str(), SW_SHOW);
+      }
+   }
+//---------------------------------------------------------------------------
+// Compile all files
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall makeFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   for (unsigned int file = 0; file != fileNames.size(); file++)
+      {
+      string command = "\"" + getApsimDirectory() + "\\bin\\apsbuild -make \"" + fileNames[file] + "\"";
+
+      // run command
+      WinExec(command.c_str(), SW_SHOW);
+      }
+   }
+//---------------------------------------------------------------------------
+// Build all files.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall buildFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   for (unsigned int file = 0; file != fileNames.size(); file++)
+      {
+      string command = "\"" + getApsimDirectory() + "\\bin\\apsbuild -build \"" + fileNames[file] + "\"";
+
+      // run command
+      WinExec(command.c_str(), SW_SHOW);
+      }
+   }
+
+//---------------------------------------------------------------------------
+// Send all files to an editor.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall viewFiles(const char* csvFiles)
+   {
+   vector<string> fileNames;
+   Split_string(csvFiles, ",", fileNames);
+
+   for (unsigned int file = 0; file != fileNames.size(); file++)
+      {
+      string command = "\"" + getApsimDirectory() + "\\bin\\apsbuild -build \"" + fileNames[file] + "\"";
+
+      // run command
+      ShellExecute(NULL, "open", fileNames[file].c_str(), NULL, "", SW_SHOW);
+      }
+   }
+
