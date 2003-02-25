dcd4bd0a1503b045f106572bf592f0f6bebd9f8f
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-02-25 00:07:15 +0000

    no message

---
+++ ShellExt/source/ApsimCommands.h
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+
+#ifndef ApsimCommandsH
+#define ApsimCommandsH
+//---------------------------------------------------------------------------
+// Send all files to EXCEL.  Files is a CSV list of filenames.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall excelFiles(const char* csvFiles);
+
+//---------------------------------------------------------------------------
+// Send all files to APSVis.  Files is a CSV list of filenames.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall apsvisFiles(const char* csvFiles);
+
+//---------------------------------------------------------------------------
+// Send all files to Apsim Outlook.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall apsimoutlookFiles(const char* csvFiles);
+
+//---------------------------------------------------------------------------
+// Send all files to Apsim.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall runFiles(const char* csvFiles);
+
+//---------------------------------------------------------------------------
+// Convert all files to SIM format.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall createSimFiles(const char* csvFiles);
+
+//---------------------------------------------------------------------------
+// Compile all files
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall makeFiles(const char* csvFiles);
+
+//---------------------------------------------------------------------------
+// Build all files.
+//---------------------------------------------------------------------------
+extern "C" _export void __stdcall buildFiles(const char* csvFiles);
+
+
+#endif
