3ffcb673e2d3c1a7fbd7fb76d42210e429a0e8ec
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-06-19 00:25:47 +0000

    Changed to do refresh better

--- Shared/SEGReport/TSEGTable.cpp
+++ Shared/SEGReport/TSEGTable.cpp
@@ -6,6 +6,8 @@
 #include "TSEGTable.h"
 #include <general\stl_functions.h>
 #include <general\db_functions.h>
+#include <general\vcl_functions.h>
+#include <general\StringTokenizer.h>
 using namespace std;
 
 #pragma package(smart_init)
@@ -20,15 +22,16 @@ __fastcall TSEGTable::TSEGTable(TComponent* Owner)
    : TkbmMemTable(Owner)
    {
    source = NULL;
-   afterDataRefresh = NULL;
    BeforeOpen = beforeOpen;
    AfterOpen = afterOpen;
+   subscriptionComponents = new TStringList;
    }
 // ------------------------------------------------------------------
 // destructor
 // ------------------------------------------------------------------
 __fastcall TSEGTable::~TSEGTable()
    {
+   delete subscriptionComponents;
    }
 //---------------------------------------------------------------------------
 // The component has been loaded from the stream - go refresh ourselves.
@@ -38,12 +41,20 @@ void __fastcall TSEGTable::Loaded(void)
    try
       {
       TkbmMemTable::Loaded();
+      fixupSubReferences();
       refresh();
       }
    catch (const Exception& error)
       {
       Application->MessageBox(error.Message.c_str(), "Error", MB_ICONSTOP | MB_OK);
       }
+   }
+// ------------------------------------------------------------------
+// Set the subscription component names.
+// ------------------------------------------------------------------
+void __fastcall TSEGTable::setSubComponentNames(TStringList* compNames)
+   {
+   subscriptionComponents->Assign(compNames);
    }
 // ------------------------------------------------------------------
 // User has just done a Active=true - go add fields before the
@@ -55,6 +66,10 @@ void __fastcall TSEGTable::beforeOpen(TDataSet* dataset)
       {
       TCursor savedCursor = Screen->Cursor;
       Screen->Cursor = crHourGlass;
+
+      if (IndexDefs->Count > 0)
+         DeleteIndex("mainIndex");
+
       try
          {
          Active = false;
@@ -68,12 +83,12 @@ void __fastcall TSEGTable::beforeOpen(TDataSet* dataset)
          }
       catch (const exception& error)  // one of our error messages.
          {
-         ::MessageBox(NULL, error.what(), "Error", MB_ICONSTOP | MB_OK);
+         errors().insert(error.what());
          Active = false;
          }
       catch (const Exception& error)  // VCL error
          {
-         ::MessageBox(NULL, error.Message.c_str(), "Error", MB_ICONSTOP | MB_OK);
+         errors().insert(error.Message.c_str());
          Active = false;
          }
       Screen->Cursor = savedCursor;
@@ -90,13 +105,23 @@ void __fastcall TSEGTable::afterOpen(TDataSet* dataset)
    Screen->Cursor = crHourGlass;
    try
       {
-      if (IndexDefs->Count > 0)
-         DeleteIndex("mainIndex");
-
       // Go load all records.
       storeRecords();
-      if (afterDataRefresh != NULL)
-         afterDataRefresh(this);
+
+      // Create an index so that the range methods work.
+      if (IndexDefs->Count == 0)
+         {
+         AnsiString indexFields = AnsiString(SERIES_FIELD_NAME);
+         AddIndex("mainIndex", indexFields, TIndexOptions());
+         IndexFieldNames = indexFields;
+         }
+      Sort(TkbmMemTableCompareOptions());
+
+      for (unsigned i = 0; i != subscriptionEvents.size(); i++)
+         {
+         if (subscriptionEvents[i] != NULL)
+            subscriptionEvents[i](this);
+         }
       EnableControls();
 
       // force children to update themselves.
@@ -106,12 +131,12 @@ void __fastcall TSEGTable::afterOpen(TDataSet* dataset)
       }
    catch (const exception& error)  // one of our error messages.
       {
-      ::MessageBox(NULL, error.what(), "Error", MB_ICONSTOP | MB_OK);
+      errors().insert(error.what());
       Active = false;
       }
    catch (const Exception& error)  // VCL error
       {
-      ::MessageBox(NULL, error.Message.c_str(), "Error", MB_ICONSTOP | MB_OK);
+      errors().insert(error.Message.c_str());
       Active = false;
       }
    EnableControls();
@@ -122,7 +147,7 @@ void __fastcall TSEGTable::afterOpen(TDataSet* dataset)
 // ------------------------------------------------------------------
 void TSEGTable::refresh (void)
    {
-   if (!ComponentState.Contains(csLoading))
+   if (!ComponentState.Contains(csLoading) && source == NULL)
       {
       Active = false;
       Active = true;
@@ -136,11 +161,17 @@ void __fastcall TSEGTable::setSourceDataset(TSEGTable* source)
    {
    if (source != sourceDataset)
       {
+      if (sourceDataset != NULL)
+         sourceDataset->removeDataChangeSubscription(Name.c_str());
       sourceDataset = source;
-      refresh();
+      sourceDataset->addDataChangeSubscription(Name + ".onSourceDataChanged");
+      if (!ComponentState.Contains(csLoading))
+         {
+         Active = false;
+         Active = true;
+         }
       }
    }
-
 // ------------------------------------------------------------------
 // return the name of the year field name.
 // ------------------------------------------------------------------
@@ -162,6 +193,7 @@ string TSEGTable::getYearFieldName(void) const throw(runtime_error)
 // ------------------------------------------------------------------
 void TSEGTable::getSeriesNames(vector<string>& seriesNames)
    {
+   seriesNames.erase(seriesNames.begin(), seriesNames.end());
    First();
    while (!Eof)
       {
@@ -246,5 +278,63 @@ bool TSEGTable::nextSeries(void)
 void TSEGTable::cancelSeries(void)
    {
    CancelRange();
+   }
+// ------------------------------------------------------------------
+// Add a subscription to the data changed event.
+// ------------------------------------------------------------------
+void TSEGTable::addDataChangeSubscription(AnsiString eventName)
+   {
+   int index = subscriptionComponents->IndexOf(eventName);
+   if (index == -1)
+      index = subscriptionComponents->Add(eventName);
+   while (index >= subscriptionEvents.size())
+      subscriptionEvents.push_back((TDataSetNotifyEvent)NULL);
+
+   union
+      {
+      TDataSetNotifyEvent event;
+      TMethod     asPointers;
+      } u;
+   StringTokenizer tokenizer(eventName.c_str(), ".");
+   string compName = tokenizer.nextToken();
+   string event = tokenizer.nextToken();
+   TComponent* subComponent = getComponent<TComponent> (Owner, compName.c_str());
+   if (subComponent != NULL)
+      {
+      u.asPointers.Code = subComponent->MethodAddress(event.c_str());
+      u.asPointers.Data = subComponent;
+      subscriptionEvents[index] = u.event;
+      }
+   }
+// ------------------------------------------------------------------
+// Remove a subscription to the data changed event.
+// ------------------------------------------------------------------
+void TSEGTable::removeDataChangeSubscription(AnsiString eventName)
+   {
+   int pos = subscriptionComponents->IndexOf(eventName);
+   if (pos >= 0)
+      {
+      subscriptionComponents->Delete(pos);
+      subscriptionEvents.erase(subscriptionEvents.begin() + pos);
+      }
+   }
+// ------------------------------------------------------------------
+// Event handler called when the source dataset changes.
+// ------------------------------------------------------------------
+void __fastcall TSEGTable::onSourceDataChanged(TDataSet* dataset)
+   {
+   Active = false;
+   Active = true;
+   }
+// ------------------------------------------------------------------
+// Fixup all subscription references.
+// ------------------------------------------------------------------
+void TSEGTable::fixupSubReferences(void)
+   {
+   for (int i = 0; i != subscriptionComponents->Count; i++)
+      {
+      subscriptionEvents.push_back((TDataSetNotifyEvent)NULL);
+      addDataChangeSubscription(subscriptionComponents->Strings[i]);
+      }
    }
 
