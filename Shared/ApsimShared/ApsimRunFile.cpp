d415427f19b20db9333d100eaf0f8de845d4b3b9
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-06-30 02:28:23 +0000

    Moved all apsim running to ApsimRuns in APSRun directory

--- Shared/ApsimShared/ApsimRunFile.cpp
+++ Shared/ApsimShared/ApsimRunFile.cpp
@@ -12,67 +12,38 @@ using namespace std;
 #pragma package(smart_init)
 
 static const char* SIMULATION_FILE_KEY = "Simulation_file";
-static const char* CONFIGURATION_FILE_KEY  = "Configuration_file";
 static const char* SIMULATION_KEY   = "Simulation";
 
 // ------------------------------------------------------------------
 // Constructor
 // ------------------------------------------------------------------
 ApsimRunFile::ApsimRunFile(const std::string& file)
-   : fileName(file)
    {
+   ini = new IniFile(file);
    }
-
 // ------------------------------------------------------------------
-// Run this run file
+// destructor.
 // ------------------------------------------------------------------
-void ApsimRunFile::run(bool console) const
+ApsimRunFile::~ApsimRunFile()
    {
-   IniFile ini(fileName);
-   vector<string> sections;
-   ini.readSectionNames(sections);
-   for (vector<string>::iterator sectionI = sections.begin();
-                                 sectionI != sections.end();
-                                 sectionI++)
-      {
-      string simulationFileName, configFileName;
-      vector<string> simulations;
-
-      ini.read(*sectionI, SIMULATION_FILE_KEY, simulationFileName);
-      ini.read(*sectionI, CONFIGURATION_FILE_KEY, configFileName);
-      ini.read(*sectionI, SIMULATION_KEY, simulations);
-
-      if (ExtractFileExt(simulationFileName.c_str()).AnsiCompareIC(".sim") == 0)
-         {
-         ApsimSimulationFile::run(simulationFileName, console);
-         }
-      else
-         {
-         if(simulations.size() == 0)
-            ApsimControlFile::getAllSectionNames(simulationFileName, simulations);
-         for (vector<string>::iterator sim = simulations.begin();
-                                       sim != simulations.end();
-                                       sim++)
-            {
-            try
-               {
-               ApsimControlFile simulation(simulationFileName, *sim);
-               HANDLE childProcess = simulation.run(configFileName, console);
-               DWORD exitCode;
-               GetExitCodeProcess(childProcess, &exitCode);
-               while (exitCode == STILL_ACTIVE)
-                  {
-                  Application->ProcessMessages();
-                  GetExitCodeProcess(childProcess, &exitCode);
-                  }
-               }
-            catch (const runtime_error& err)
-               {
-               ::MessageBox(NULL, err.what(), "Error", MB_OK);
-               }
-            }
-         }
-      }
+   delete ini;
+   }
+// ------------------------------------------------------------------
+// Return a list of runs to caller.
+// ------------------------------------------------------------------
+void ApsimRunFile::getRuns(std::vector<std::string>& runNames) const
+   {
+   ini->readSectionNames(runNames);
+   }
+// ------------------------------------------------------------------
+// Return a specified run to caller.
+// ------------------------------------------------------------------
+void ApsimRunFile::getRun(const std::string& runName,
+                          std::string& fileName,
+                          std::vector<std::string>& conFileSections) const
+   {
+   ini->read(runName, SIMULATION_FILE_KEY, fileName);
+   ini->read(runName, SIMULATION_KEY, conFileSections);
    }
 
 
