939ff057887b9b25c8b5cc631cad22296a71a6a6
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-07-31 06:15:12 +0000

    Allowed column resizing and fixed number of decimal places

--- trunk/ApsimReport/Source/TDataPreviewForm.h
+++ trunk/ApsimReport/Source/TDataPreviewForm.h
@@ -18,6 +18,8 @@ class TDataPreviewForm : public TForm
 __published:	// IDE-managed Components
    TDataSource *DataSource;
    TDBAdvStringGrid *DBAdvStringGrid1;
+   void __fastcall DBAdvStringGrid1GetFloatFormat(TObject *Sender,
+          int ACol, int ARow, bool &IsFloat, AnsiString &FloatFormat);
 private:	// User declarations
 public:		// User declarations
    __fastcall TDataPreviewForm(TComponent* Owner);
