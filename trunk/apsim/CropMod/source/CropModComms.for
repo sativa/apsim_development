1429b37722d4d8c2c4befca375d2d44754fdfb17
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-08-19 04:18:33 +0000

    C-411 EP output is now a positive number

--- trunk/apsim/CropMod/source/CropModComms.for
+++ trunk/apsim/CropMod/source/CropModComms.for
@@ -685,6 +685,7 @@ cjh      endif
       REAL       plant_p_max
       REAL       pconc
       REAL       value
+      REAL       ep
 
       REAL       no3_uptake(max_layer)
       REAL       nh4_uptake(max_layer)
@@ -1377,13 +1378,20 @@ cjh      endif
      :                             , g%swdef_tiller)
 
 
-      elseif ((variable_name .eq. 'sw_uptake').or.
-     :        (variable_name .eq. 'ep'))        then
+      elseif (variable_name .eq. 'sw_uptake') then
          num_layers = count_of_real_vals (g%dlayer, max_layer)
          call respond2get_real_array (variable_name
      :                               , '(mm)'
      :                               , g%dlt_sw_dep
      :                               , num_layers)
+
+      elseif (variable_name .eq. 'ep') then
+         num_layers = count_of_real_vals (g%dlayer, max_layer)
+         ep = -sum(g%dlt_sw_dep(1:num_layers))
+
+         call respond2get_real_var (variable_name
+     :                               , '(mm)'
+     :                               , ep)
 
       elseif (variable_name .eq. 'transpiration') then
          deepest_layer = find_layer_no (g%root_depth, g%dlayer
