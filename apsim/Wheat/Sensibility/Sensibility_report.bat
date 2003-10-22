4c2df4b8a171ead0b647cb03b0601e5967f570b0
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-10-22 00:58:38 +0000

    no message

---
+++ apsim/Wheat/Sensibility/Sensibility_report.bat
@@ -0,0 +1,6 @@
+pushd
+cd \development\apsim\wheat\sensibility
+c:\development\bin\apsimreport.exe nsw.report nsw.jpg
+c:\development\bin\apsimreport.exe birchip.report birchip.jpg
+c:\development\bin\apsimreport.exe warra.report warra.jpg
+popd
