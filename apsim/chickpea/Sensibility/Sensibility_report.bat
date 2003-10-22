b1e9db5559a6dbb64005daf797aea7d0bcc74dca
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-10-22 00:06:17 +0000

    Added sensibility report stuff

---
+++ apsim/chickpea/Sensibility/Sensibility_report.bat
@@ -0,0 +1,4 @@
+pushd
+cd \development\apsim\chickpea\sensibility
+c:\development\bin\apsimreport.exe chickpea.report chickpea.jpg
+popd
---
+++ apsim/maize/Sensibility/Sensibility_report.bat
@@ -0,0 +1,5 @@
+pushd
+cd \development\apsim\maize\sensibility
+c:\development\bin\apsimreport.exe dalby.report dalby.jpg
+c:\development\bin\apsimreport.exe roma.report roma.jpg
+popd
