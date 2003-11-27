60645af794f5c208c8053c35c57caa98600af57d
nightly_build <nightly_build@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-11-27 05:43:04 +0000

    no message

--- apsim/maize/Sensibility/Sensibility_report.bat
+++ apsim/maize/Sensibility/Sensibility_report.bat
@@ -1,5 +1,5 @@
 pushd
 cd \development\apsim\maize\sensibility
-c:\development\bin\apsimreport.exe dalby.report dalby.jpg
-c:\development\bin\apsimreport.exe roma.report roma.jpg
+c:\development\bin\apsimreport.exe dalby.report dalby.pdf
+c:\development\bin\apsimreport.exe roma.report roma.pdf
 popd
