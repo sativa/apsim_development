367c57fa4abbb6160f2a7ca2a8dd06bb94755784
nightly_build <nightly_build@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2003-11-27 04:39:48 +0000

    no message

--- trunk/apsim/Wheat/Sensibility/Sensibility_report.bat
+++ trunk/apsim/Wheat/Sensibility/Sensibility_report.bat
@@ -3,4 +3,5 @@ cd \development\apsim\wheat\sensibility
 c:\development\bin\apsimreport.exe nsw.report nsw.pdf
 c:\development\bin\apsimreport.exe birchip.report birchip.pdf
 c:\development\bin\apsimreport.exe warra.report warra.pdf
+c:\development\bin\apsimreport.exe DarlingDowns.report DarlingDowns.pdf
 popd
