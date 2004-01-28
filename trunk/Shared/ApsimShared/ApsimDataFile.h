49de81b08139799cc83830d66b2530043924ad53
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-01-28 23:31:39 +0000

    New implementation

--- trunk/Shared/ApsimShared/ApsimDataFile.h
+++ trunk/Shared/ApsimShared/ApsimDataFile.h
@@ -1,37 +1,93 @@
 //---------------------------------------------------------------------------
 #ifndef ApsimDataFileH
 #define ApsimDataFileH
-#include <stdexcept>
-#include <fstream>
 #include <map>
+#include <vector>
+#include <fstream>
+#include <boost\date_time\gregorian\gregorian.hpp>
+
 //---------------------------------------------------------------------------
-// derived from TSEGTable, this class reads all data from 1 or more
-// APSIM output files.
+// This class a single 'value' whether it be a constant or a value for
+// a specific date. NB A 'value' can have multiple values ie an array.
+//---------------------------------------------------------------------------
+struct Value
+   {
+   std::string name;
+   std::string units;
+   std::vector<std::string> values;
+   std::string comment;
+   Value(void) { }
+   Value(const std::string& n, const std::string& u,
+         const std::vector<std::string>& v, const std::string& c)
+      : name(n), units(u), values(v), comment(c) { }
+   Value(const std::string& n, const std::string& u,
+         const std::string& v, const std::string& c)
+      : name(n), units(u), comment(c) {values.push_back(v);}
+   std::string getName(void) {return name;}
+   bool operator== (const std::string& rhsName)
+      {return (strcmpi(name.c_str(), rhsName.c_str()) == 0);}
+   };
+
+//---------------------------------------------------------------------------
+// This class encapsulates all reading from an APSIM data file.
+// A data file consists of 2 sections, a constants section and a temporal
+// data section. Comments begin with an exclamation character '!'
+//
+// The constants section consists of lines like: keyword = value (units).
+// e.g. Latitude =  -27.11
+//      tav =  19.57 (oC)
+// Units are optional. There is no limit to the number of constants.
+// A constant can be an array of values:
+// e.g. sw = 0.9 0.2 0.5 (mm/mm)
+//
+// The temporal data section contains fields (columns) of data for
+// specific dates. Each field has a field name and units on separate lines:
+// e.g. site year   day  radn   maxt   mint   rain    evap
+//        ()   ()    () (MJ/m2) (oC)   (oC)   (mm)    (mm)
+//      DALB 1988     1 20.74   33.0   17.4    0.2    7.41
+// Each field must have a name and unit. There is no limit to the number
+// or types of fields.
+// Arrays are supported:
+// sw(1)  sw(2)  sw(3)
+//  (mm)   (mm)   (mm)
+//   0.8    0.6   0.55
+// Each record must have a date. The date can be represented as either:
+//     year, month and day columns
+//     year and day columns where day is day of year
+//     date column in the ISO 8601 extended format: YYYY-MM-DD
 //---------------------------------------------------------------------------
 class __declspec(dllexport) ApsimDataFile
    {
    public:
-      ApsimDataFile(const std::string& filename) throw(std::runtime_error);
+      typedef std::vector<Value> Values;
+      typedef Values::iterator iterator;
 
-      // return a list of field names to caller.
-      void getFieldNames(std::vector<std::string>& fieldNames) const;
+      //---------------------------------------------------------------------------
+      // constructor.
+      //---------------------------------------------------------------------------
+      ApsimDataFile(void) { }
 
-      // return a list of field names to caller.
-      void getFieldUnits(std::vector<std::string>& fieldUnits) const;
+      //---------------------------------------------------------------------------
+      // Open the data file.
+      //---------------------------------------------------------------------------
+      void open(const std::string& fileName);
 
-      // return the values on the current record.
-      void getFieldValues(std::vector<std::string>& fieldValues) const;
+      //---------------------------------------------------------------------------
+      // constant iterators
+      //---------------------------------------------------------------------------
+      iterator constantsBegin() {return constants.begin();}
+      iterator constantsEnd()   {return constants.end();}
 
+      //---------------------------------------------------------------------------
+      // temporal iterators
+      //---------------------------------------------------------------------------
+      iterator fieldsBegin() {return temporalData.begin();}
+      iterator fieldsEnd()   {return temporalData.end();}
+
+      //---------------------------------------------------------------------------
       // return the date on the current record.
-      TDateTime getDate(void) const throw(std::runtime_error);
-
-      // return a single field value for the specified field name.  Returns
-      // a blank string if not found.
-      std::string getFieldValue(const std::string& fieldName) const;
-
-      // return a single field value for the specified field index.  Throws
-      // on an invalid index.
-      std::string getFieldValue(unsigned fieldIndex) const throw(std::runtime_error);
+      //---------------------------------------------------------------------------
+      boost::gregorian::date getDate(void) const;
 
       // position at first record.
       bool first(void);
@@ -42,34 +98,28 @@ class __declspec(dllexport) ApsimDataFile
       // advance to the last record.
       bool last(void);
 
-      // return a list of constant names to caller.
-      void getConstantNames(std::vector<std::string>& names) const;
-
-      // return the value of the specified constant.  Returns a blank
-      // string if constant doesn't exist.
-      std::string getConstant(const std::string& name) const;
+      // return true if at end of file.
+      bool eof(void);
 
    private:
       std::string fileName;
-      std::ifstream in;
-      std::vector<std::string> fieldNames;
-      std::vector<std::string> fieldUnits;
-      std::vector<std::string> fieldValues;
-      typedef std::map<std::string, std::string, std::less<std::string> > Constants;
-      Constants constants;
-      unsigned firstRecordPos;
-      unsigned yearI;
-      unsigned dayOfYearI;
-      unsigned dayOfMonthI;
-      unsigned monthI;
+      Values constants;
+      Values temporalData;
+      std::vector<unsigned> columnIndexes;
 
-      void open(void) throw(std::runtime_error);
+      std::ifstream in;
+      unsigned firstRecordPos;
+      iterator yearI;
+      iterator monthI;
+      iterator dayI;
+      iterator dateI;
+
       std::istream& getline(std::string& line);
 
       void readAndStoreFields(const std::string& filename) throw(std::runtime_error);
       void readAndStoreRecords(const std::string& filename) throw (std::runtime_error);
-      void readApsimHeader(std::istream& in) throw(std::runtime_error);
-      bool readNextRecord(std::istream& in) throw(std::runtime_error);
+      void readApsimHeader() throw(std::runtime_error);
+      bool readNextRecord() throw(std::runtime_error);
       void lookForDateField(void);
 
    };
