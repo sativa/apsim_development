827112b3ae47a8b95237f7326a9d082f6e7cd5cd
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-01-28 23:31:39 +0000

    New implementation

--- Shared/ApsimShared/ApsimDataFile.cpp
+++ Shared/ApsimShared/ApsimDataFile.cpp
@@ -1,6 +1,5 @@
 //---------------------------------------------------------------------------
 #include <general\pch.h>
-#include <vcl.h>
 #pragma hdrstop
 
 #include "ApsimDataFile.h"
@@ -9,31 +8,30 @@
 #include <general\string_functions.h>
 #include <general\stl_functions.h>
 #include <general\inifile.h>
+#include <boost\filesystem\operations.hpp>
+#include <boost\lexical_cast.hpp>
+#include <boost\date_time\date_parsing.hpp>
 #include "fstring.h"
 #pragma package(smart_init)
 
 using namespace std;
+using namespace boost;
+using namespace boost::filesystem;
+using namespace boost::gregorian;
 
-//---------------------------------------------------------------------------
-// constructor
-//---------------------------------------------------------------------------
-ApsimDataFile::ApsimDataFile(const string& filename)  throw(runtime_error)
-   : fileName(filename)
-   {
-   open();
-   }
 //---------------------------------------------------------------------------
 // read in the contents of the specified file into this TSEGTable.
 //---------------------------------------------------------------------------
-void ApsimDataFile::open(void) throw(runtime_error)
+void ApsimDataFile::open(const std::string& file)
    {
-   if (!FileExists(fileName.c_str()))
+   fileName = file;
+   if (!exists(path(fileName, native)))
       throw runtime_error("Cannot open file: " + fileName + ". File doesn't exist");
 
    in.open(fileName.c_str(), ios::binary);
-   readApsimHeader(in);
+   readApsimHeader();
    firstRecordPos = in.tellg();
-   readNextRecord(in);
+   readNextRecord();
    lookForDateField();
    }
 //---------------------------------------------------------------------------
@@ -44,65 +42,15 @@ istream& ApsimDataFile::getline(string& line)
    std::getline(in, line);
    if (line[line.size()-1] == '\r')
       line.erase(line.size()-1);
-   Strip(line, " ");
+   stripLeadingTrailing(line, " ");
    return in;
-   }
-//---------------------------------------------------------------------------
-// return a list of field names to caller.
-//---------------------------------------------------------------------------
-void ApsimDataFile::getFieldNames(std::vector<std::string>& names) const
-   {
-   names = fieldNames;
-   }
-//---------------------------------------------------------------------------
-// return a list of field units to caller.
-//---------------------------------------------------------------------------
-void ApsimDataFile::getFieldUnits(std::vector<std::string>& units) const
-   {
-   units = fieldUnits;
-   }
-//---------------------------------------------------------------------------
-// return a list of field values to caller.
-//---------------------------------------------------------------------------
-void ApsimDataFile::getFieldValues(std::vector<std::string>& values) const
-   {
-   values = fieldValues;
-   }
-//---------------------------------------------------------------------------
-// return a single field value for the specified field name.  Returns
-// a blank string if not found.
-//---------------------------------------------------------------------------
-std::string ApsimDataFile::getFieldValue(const std::string& fieldName) const
-   {
-   vector<string>::const_iterator fieldI = find(fieldNames.begin(),
-                                                fieldNames.end(),
-                                                fieldName);
-   if (fieldI == fieldNames.end())
-      return "";
-   else
-      return fieldValues[fieldI - fieldNames.begin()];
-   }
-//---------------------------------------------------------------------------
-// return a single field value for the specified field index.  Throws
-// on an invalid index.
-//---------------------------------------------------------------------------
-std::string ApsimDataFile::getFieldValue(unsigned fieldIndex) const throw(runtime_error)
-   {
-   if (fieldIndex >= fieldValues.size())
-      {
-      string msg = "Invalid field index: ";
-      msg += IntToStr(fieldIndex).c_str();
-      msg += " while trying to reference file: " + fileName;
-      throw runtime_error(msg);
-      }
-   return fieldValues[fieldIndex];
    }
 //---------------------------------------------------------------------------
 // advance to the next record.
 //---------------------------------------------------------------------------
 bool ApsimDataFile::next(void)
    {
-   return readNextRecord(in);
+   return readNextRecord();
    }
 //---------------------------------------------------------------------------
 // position at first record.
@@ -111,7 +59,7 @@ bool ApsimDataFile::first(void)
    {
    in.clear();
    in.seekg(firstRecordPos);
-   return readNextRecord(in);
+   return readNextRecord();
    }
 //---------------------------------------------------------------------------
 // advance to the last record.
@@ -131,91 +79,132 @@ bool ApsimDataFile::last(void)
 
    // move forward 1 char to get past the \n
    in.seekg(1, ios::cur);
-   return readNextRecord(in);
-   }
-//---------------------------------------------------------------------------
-// return a list of constant names to caller.
-//---------------------------------------------------------------------------
-void ApsimDataFile::getConstantNames(std::vector<std::string>& names) const
-   {
-   for (Constants::const_iterator i = constants.begin();
-                                  i != constants.end();
-                                  i++)
-      names.push_back(i->first);
-   }
-//---------------------------------------------------------------------------
-// return the value of the specified constant.  Returns a blank
-// string if constant doesn't exist.
-//---------------------------------------------------------------------------
-string ApsimDataFile::getConstant(const string& name) const
-   {
-   Constants::const_iterator i = constants.find(name);
-   if (i == constants.end())
-      return "";
-   else
-      return i->second;
+   return readNextRecord();
    }
 // ------------------------------------------------------------------
 // read in the header part of an apsim output file.  The header part
 // consists of a title= line, followed by a line of field names
 // and a line of unit names.
 // ------------------------------------------------------------------
-void ApsimDataFile::readApsimHeader(istream& in) throw(runtime_error)
+void ApsimDataFile::readApsimHeader() throw(runtime_error)
    {
    // loop through all lines looking for heading line.
    string line, previousLine;
    bool foundHeadings = false;
    while (!foundHeadings && getline(line))
       {
-      unsigned posComment = line.find('!');
-      if (posComment != string::npos)
-         line.erase(posComment);
-
-      string key, value;
-      getKeyNameAndValue(line, key, value);
-      if (key != "")
-         constants.insert(Constants::value_type(key, value));
-
-      // If the first non-blank character on the line is a open bracket '('
-      // then we have found the units line.  The previous line is then
-      // assumed to be the headings line.
-      unsigned int posFirstNonBlankChar = line.find_first_not_of (" ");
-      if (posFirstNonBlankChar != string::npos &&
-          line[posFirstNonBlankChar] == '(')
+      if (line.find('=') != string::npos)
          {
-         foundHeadings = true;
-         Split_string(line, " ", fieldUnits);
-         Split_string(previousLine, " ", fieldNames);
+         Value value;
+         value.comment = splitOffAfterDelimiter(line, "!");
+         stripLeadingTrailing(value.comment, " ");
 
-         // check for duplicate fields
-         vector<string> temp;
-         copy(fieldNames.begin(), fieldNames.end(), back_inserter(temp));
-         sort(temp.begin(), temp.end());
-         if (unique(temp.begin(), temp.end()) != temp.end())
-            throw runtime_error("Non-unique column names encountered.  "
-                        "An APSIM output file must not have two columns with "
-                        "the same name.");
+         string valueString;
+         getKeyNameAndValue(line, value.name, valueString);
+         if (value.name != "")
+            {
+            stripLeadingTrailing(value.name, " ");
+            value.units = "(" + splitOffBracketedValue(valueString, '(', ')') + ")";
+            splitIntoValues(valueString, " ", value.values);
+            constants.push_back(value);
+            }
+         }
+      else
+         {
+         splitOffAfterDelimiter(line, "!");
+
+         // If the first non-blank character on the line is a open bracket '('
+         // then we have found the units line.  The previous line is then
+         // assumed to be the headings line.
+         unsigned int posFirstNonBlankChar = line.find_first_not_of (" ");
+         if (posFirstNonBlankChar != string::npos &&
+             line[posFirstNonBlankChar] == '(')
+            {
+            foundHeadings = true;
+
+            vector<string> fieldNames, fieldUnits;
+            splitIntoValues(line, " ", fieldUnits);
+            splitIntoValues(previousLine, " ", fieldNames);
+
+            if (fieldNames.size() != fieldUnits.size())
+               throw runtime_error("Each column in an APSIM input data file must "
+                                   "have a unit and a heading.\n"
+                                   "Filename = " + fileName);
+
+            // check for duplicate fields
+            vector<string> temp;
+            copy(fieldNames.begin(), fieldNames.end(), back_inserter(temp));
+            sort(temp.begin(), temp.end());
+            if (unique(temp.begin(), temp.end()) != temp.end())
+               throw runtime_error("Column names in an APSIM input file must be"
+                                   "unique\n"
+                                   "Filename = " + fileName);
+            // The problem now is that some headings may contain array
+            // indexes so for each column we are going to build up a container
+            // of pointers to temporal Value objects. This will make it easier
+            // later to append a string value to a value object when we do a
+            // getline.
+            for (unsigned i = 0; i != fieldNames.size(); i++)
+               {
+               static unsigned previousArrayIndex = 0;
+               string arrayIndexString = splitOffBracketedValue(fieldNames[i], '(', ')');
+               bool newValue;
+               if (arrayIndexString == "")
+                  newValue = true;
+               else
+                  {
+                  unsigned arrayIndex = lexical_cast<unsigned>(arrayIndexString);
+                  if (arrayIndex <= 0 || (arrayIndex > 1 && arrayIndex != previousArrayIndex+1))
+                     throw runtime_error("Invalid array index: " + arrayIndexString
+                                         + " for column: " + fieldNames[i]);
+                  newValue = (arrayIndex == 1);
+                  previousArrayIndex = arrayIndex;
+                  }
+               static Value value;
+               if (newValue)
+                  {
+                  value.name = fieldNames[i];
+                  value.units = fieldUnits[i];
+                  temporalData.push_back(value);
+                  previousArrayIndex = 1;
+                  }
+               columnIndexes.push_back(temporalData.size()-1);
+               }
+            }
          }
       previousLine = line;
       }
    }
 // ------------------------------------------------------------------
+// Clear all values from the specified temporal object.
+// ------------------------------------------------------------------
+void ClearValues(Value& value)
+   {
+   value.values.erase(value.values.begin(), value.values.end());
+   }
+// ------------------------------------------------------------------
 // Read in the next record.  Return true if values are returned.
 // ------------------------------------------------------------------
-bool ApsimDataFile::readNextRecord(istream& in) throw(runtime_error)
+bool ApsimDataFile::readNextRecord() throw(runtime_error)
    {
+   // clear all values from all temporal objects.
+   for_each(temporalData.begin(), temporalData.end(), ClearValues);
+
    string line;
    if (getline(line) && line.length() > 0)
       {
+      vector<string> fieldValues;
+
       // remove any comment characters.
       unsigned posComment = line.find('!');
       if (posComment != string::npos)
          line.erase(posComment);
-      Split_string(line, " ", fieldValues);
-      if (fieldValues.size() != fieldNames.size())
-         {
-         throw runtime_error("Not enough values on line: " + line);
-         }
+      splitIntoValues(line, " ", fieldValues);
+      if (fieldValues.size() != columnIndexes.size())
+         throw runtime_error("Not enough values on line: " + line + " in file: " + fileName);
+
+      for (unsigned i = 0; i != columnIndexes.size(); i++)
+         temporalData[columnIndexes[i]].values.push_back(fieldValues[i]);
       return true;
       }
    else
@@ -227,69 +216,73 @@ bool ApsimDataFile::readNextRecord(istream& in) throw(runtime_error)
 // ------------------------------------------------------------------
 void ApsimDataFile::lookForDateField(void)
    {
-   vector<string>::iterator i;
-
-   // look for year column
-   i = find_if(fieldNames.begin(), fieldNames.end(),
-               CaseInsensitiveStringComparison("year"));
-   if (i != fieldNames.end())
-      yearI = i - fieldNames.begin();
-
-   // look for day column
-   i = find_if(fieldNames.begin(),
-               fieldNames.end(),
-               CaseInsensitiveStringComparison("day"));
-   if (i != fieldNames.end())
-      dayOfYearI = i - fieldNames.begin();
-
-   // look for day_of_month column
-   i = find_if(fieldNames.begin(),
-               fieldNames.end(),
-               CaseInsensitiveStringComparison("day_of_month"));
-   if (i != fieldNames.end())
-      dayOfMonthI = i - fieldNames.begin();
-
-   // look for month column
-   i = find_if(fieldNames.begin(),
-               fieldNames.end(),
-               CaseInsensitiveStringComparison("month"));
-   if (i != fieldNames.end())
-      monthI = i - fieldNames.begin();
+   yearI = temporalData.end();
+   monthI = temporalData.end();
+   dayI = temporalData.end();
+   dateI = temporalData.end();
+   for (iterator i = temporalData.begin();
+                 i != temporalData.end();
+                 i++)
+      {
+      if (Str_i_Eq(i->name, "year"))
+         yearI = i;
+      else if (Str_i_Eq(i->name, "month"))
+         monthI = i;
+      else if (Str_i_Eq(i->name, "day"))
+         dayI = i;
+      else if (Str_i_Eq(i->name, "date"))
+         dateI = i;
+      }
+   bool ok = (dateI != temporalData.end());
+   if (!ok)
+      ok = (yearI != temporalData.end() && dayI != temporalData.end());
+   if (!ok)
+      ok = (dayI != temporalData.end());   // to keep patchinput's data files ok.
+   if (!ok)
+      throw runtime_error("Cannot find date columns in file: " + fileName + "\n"
+                          "The file must have one of the following combinations:\n"
+                          "   a date column\n"
+                          "   a year and day column\n"
+                          "   a year, month and day column");
    }
 // ------------------------------------------------------------------
 // return the date on the current record.
 // ------------------------------------------------------------------
-TDateTime ApsimDataFile::getDate(void) const throw(std::runtime_error)
+gregorian::date ApsimDataFile::getDate(void) const
    {
-   bool ok = (yearI != NULL &&
-           (dayOfYearI != NULL ||
-            (dayOfMonthI != NULL && monthI != NULL)));
-   if (!ok)
-      throw runtime_error("APSIM input files must have year and day OR day, "
-                          "month and year columns.");
-
-   int year = StrToInt(fieldValues[yearI].c_str());
-
-   if (dayOfYearI != NULL)
-      {
-      int dayOfYear = StrToInt(fieldValues[dayOfYearI].c_str());
-      TDateTime date(year, 1, 1);
-      return date + dayOfYear - 1;
-      }
+   if (dateI != temporalData.end())
+      return date(from_string(dateI->values[0]));
    else
       {
-      int day = StrToInt(fieldValues[dayOfMonthI].c_str());
-      int month = StrToInt(fieldValues[monthI].c_str());
-      return TDateTime(year, month, day);
+      int year = lexical_cast<int>(yearI->values[0]);
+      int day  = lexical_cast<int>(dayI->values[0]);
+      if (monthI == temporalData.end())
+         return date(year, 1, 1) + date_duration(day-1);
+      else
+         {
+         int month  = lexical_cast<int>(monthI->values[0]);
+         return date(year, month, day);
+         }
       }
    }
+//---------------------------------------------------------------------------
+// return true if at end of file.
+//---------------------------------------------------------------------------
+bool ApsimDataFile::eof(void)
+   {
+   return !in;
+   }
+
+
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
 extern "C" unsigned _export __stdcall newApsimDataFile
    (const char* filename, unsigned filenameLength)
    {
    string fileName(filename, filenameLength);
-   return (unsigned) new ApsimDataFile(fileName);
+   ApsimDataFile* dataFile = new ApsimDataFile;
+   dataFile->open(fileName);
+   return (unsigned) dataFile;
    }
 extern "C" void _export __stdcall deleteApsimDataFile
    (ApsimDataFile** dataFile)
@@ -301,7 +294,9 @@ extern "C" unsigned _export __stdcall ApsimDataFile_getFieldValue
    {
    try
       {
-      string valueString = (*dataFile)->getFieldValue(*fieldIndex);
+      ApsimDataFile::iterator i = (*dataFile)->fieldsBegin();
+      i += *fieldIndex;
+      string valueString = i->values[0];
       FString(value, valueLength, FORString) = valueString.c_str();
       return true;
       }
