21ab8aed313cc4eb5f0a14420239be34e2e57b50
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-02-12 06:49:13 +0000

    Changed all paths from absolute to relative

--- trunk/Shared/SEGReport/TExcel.h
+++ trunk/Shared/SEGReport/TExcel.h
@@ -14,17 +14,25 @@ class TExcel : public TSEGTable
       int xlsPageIndex;
       TStrings* xlsPageNames;
       vector<string> fieldNames;
+      AnsiString reportDirectory;
 
       void __fastcall setFilename(AnsiString filename);
       AnsiString __fastcall getPageName(void);
       void __fastcall setPageName(AnsiString pageName);
+      void relativeToAbsoluteFile(void);
+      void absoluteToRelativeFile(void);
 
+      virtual void __fastcall Loaded(void);
       virtual bool createFields(void) throw(std::runtime_error);
       virtual void storeRecords(void) throw(std::runtime_error);
 
    public:
       __fastcall TExcel(TComponent* owner);
       __fastcall ~TExcel(void);
+
+      // Called by SEGReport to give components a chance to know the current
+      // report directory.  Used by ApsimFileReader to use relative paths.
+      virtual void setReportDirectory(AnsiString reportDir);
 
       virtual void setProperty(const std::string& propertyName,
                                const std::string& propertyValue);
