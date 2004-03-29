b260ac866346ab2f102bd977fc5258a3f53baae5
devoilp <devoilp@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-03-29 05:56:24 +0000

    plantp

--- trunk/apsim/Plant/source/PlantComponent.cpp
+++ trunk/apsim/Plant/source/PlantComponent.cpp
@@ -1,11 +1,13 @@
 //---------------------------------------------------------------------------
 #include <general/pch.h>
 #include <vcl.h>
+#include <boost/function.hpp>
 #pragma hdrstop
 
 #include <ComponentInterface/Component.h>
 #include <ComponentInterface/ApsimVariant.h>
 #include <ApsimShared/ApsimComponentData.h>
+#include <ComponentInterface/Type.h>
 #include <ComponentInterface/Messages.h>
 #include <ComponentInterface/MessageDataExt.h>
 #include <ApsimShared/FStringExt.h>
@@ -84,13 +86,16 @@ void PlantComponent::onApsimGetQuery(protocol::ApsimGetQueryData& apsimGetQueryD
    {
    plant->onApsimGetQuery(apsimGetQueryData);
    }
+
 // ------------------------------------------------------------------
 // Return a variable to caller.
 // ------------------------------------------------------------------
 void PlantComponent::respondToGet(unsigned int& /*fromID*/, protocol::QueryValueData& queryData)
    {
-   plant->getVariable(queryData.ID, queryData);
+   baseInfo *v = vMap[queryData.ID];
+   if (v) v->sendVariable(this, queryData);
    }
+
 // ------------------------------------------------------------------
 // Set the value of a variable for the specified
 // variable name.  If this module owns the variable and does
@@ -101,34 +106,109 @@ bool PlantComponent::respondToSet(unsigned int& /*fromID*/, protocol::QuerySetVa
    return (plant->setVariable(setValueData.ID, setValueData));
    }
 
-
-// Register a variable to the rest of the system
-// returns an identifier for later use..
-unsigned int PlantComponent::addGettableVar(const char *systemName,
-                                            protocol::DataTypeCode myType,
-                                            bool isArray,
-                                            const char *units)
+void PlantComponent::addGettableVar(const char *systemName,
+                                    protocol::DataTypeCode type,
+                                    int length,
+                                    boost::function2<void, protocol::Component *, protocol::QueryValueData &> ptr,
+                                    const char *units,
+                                    const char *desc)
    {
-      char buffer[200];
-      // Build the xml fragment that describes this variable
-      strcpy(buffer, "<type kind=\"");
-      switch (myType)
-        {
-      	case protocol::DTint4:   {strcat(buffer, "integer4"); break;}
-      	case protocol::DTsingle: {strcat(buffer, "single"); break;}
-      	case protocol::DTboolean:{strcat(buffer, "boolean"); break;}
-      	case protocol::DTstring: {strcat(buffer, "string"); break;}
-      	default: {error("Undefined gettable var type", 1);}
-        }
-      strcat(buffer, "\" array=\"");
-      switch (isArray)
-         {
-   	 case false: {strcat(buffer, "F"); break;}
-   	 case true:  {strcat(buffer, "T"); break;}
-         }
-      strcat(buffer, "\" units=\"(");
-      strcat(buffer, units);
-      strcat(buffer, ")\"/>");
-      return (this->addRegistration(protocol::respondToGetReg, systemName, buffer));
+      unsigned int id = getReg(systemName, type, length>1, units); 
+      
+      // Add to variable map
+      fnInfo *v = new fnInfo(systemName, type, length, ptr, units, desc);
+      vMap.insert(UInt2InfoMap::value_type(id,v));
    }
 
+void PlantComponent::addGettableVar(const char *systemName,
+                                    protocol::DataTypeCode type,
+                                    int length,
+                                    void *ptr,
+                                    const char *units,
+                                    const char *desc)
+   {
+      unsigned int id = getReg(systemName, type, length>1, units); 
+      
+      // Add to variable map
+      varInfo *v = new varInfo(systemName, type, length, ptr, units, desc);
+      vMap.insert(UInt2InfoMap::value_type(id,v));
+   }
+
+// Build the xml fragment that describes this variable and publish to system
+unsigned int PlantComponent::getReg(const char *systemName,
+                                    protocol::DataTypeCode type, 
+                                    bool isArray, 
+                                    const char *units)
+   {
+   char buffer[200];
+   strcpy(buffer, "<type kind=\"");
+   switch (type)
+      {
+   	case protocol::DTint4:   {strcat(buffer, "integer4"); break;}
+   	case protocol::DTsingle: {strcat(buffer, "single"); break;}
+   	case protocol::DTboolean:{strcat(buffer, "boolean"); break;}
+   	case protocol::DTstring: {strcat(buffer, "string"); break;}
+   	default: {throw "Undefined gettable var type";}
+      }
+   strcat(buffer, "\" array=\"");
+   if  (isArray) {
+       strcat(buffer, "T");
+   } else { 
+       strcat(buffer, "F");
+   }
+   strcat(buffer, "\" units=\"(");
+   strcat(buffer, units);
+   strcat(buffer, ")\"/>");
+   return this->addRegistration(protocol::respondToGetReg, systemName, buffer);
+   }
+
+////////////////////
+baseInfo::baseInfo() { 
+	myLength = 0; 
+	myType = protocol::DTunknown; 
+}
+
+void varInfo::sendVariable(protocol::Component *systemInterface, protocol::QueryValueData& qd) 
+   {
+   switch (myType)
+      {
+      case protocol::DTdouble:
+         if (myLength == 1) 
+           systemInterface->sendVariable(qd, *(double *)myPtr);
+         else if (myLength > 1) 
+           systemInterface->sendVariable(qd,         
+             protocol::vector<double>((double *)myPtr, (double *)myPtr + myLength));
+         else
+           throw "Length = 0 in varInfo::sendVariable";  
+         break;
+      case protocol::DTsingle:
+         if (myLength == 1) 
+           systemInterface->sendVariable(qd, *(float *)myPtr);
+         else if (myLength > 1) 
+           systemInterface->sendVariable(qd,         
+             protocol::vector<float>((float *)myPtr, (float *)myPtr + myLength));
+         else
+           throw "Length = 0 in varInfo::sendVariable";  
+         break;
+      case protocol::DTint4 :
+         if (myLength == 1) 
+           systemInterface->sendVariable(qd, *(int *)myPtr);
+         else if (myLength > 1) 
+           systemInterface->sendVariable(qd,         
+             protocol::vector<int>((int *)myPtr, (int *)myPtr + myLength));
+         else
+           throw "Length = 0 in varInfo::sendVariable";  
+         break;
+      case protocol::DTstring :
+         if (myLength == 1) 
+           systemInterface->sendVariable(qd, FString(((string *)myPtr)->c_str()));
+         else if (myLength > 1) 
+           throw  "String Array not yet implemented";
+         else
+           throw "Length = 0 in varInfo::sendVariable";  
+         break;
+
+      default:
+         throw "Aiee unknown type in varInfo::sendVariable";
+      }
+   }
