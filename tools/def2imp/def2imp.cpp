5859e9e71feea1da2babeefdf746ae56d6bac27f
nightly_build <nightly_build@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-05-28 03:51:49 +0000

    no message

--- tools/def2imp/def2imp.cpp
+++ tools/def2imp/def2imp.cpp
@@ -29,7 +29,7 @@ int main (int argc, char** argv)
       if (words.size() > 0 &&
           words[0].find("@") == string::npos)
          {
-           out << "-import " << words[0].c _str() << endl;
+           out << "-import " << words[0].c_str() << endl;
          }
       else
          {
