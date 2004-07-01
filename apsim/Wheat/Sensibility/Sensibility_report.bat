90c54d81823a33e886bdc46aaf019d4b1c340742
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-07-01 04:03:24 +0000

    Added Walpeup to sensibility tests

--- apsim/Wheat/Sensibility/Sensibility_report.bat
+++ apsim/Wheat/Sensibility/Sensibility_report.bat
@@ -5,4 +5,5 @@ c:\development\bin\apsimreport.exe birchip.report birchip.pdf
 c:\development\bin\apsimreport.exe warra.report warra.pdf
 c:\development\bin\apsimreport.exe DarlingDowns.report DarlingDowns.pdf
 c:\development\bin\apsimreport.exe Mingenew.report Mingenew.pdf
+c:\development\bin\apsimreport.exe Walpeup.report Walpeup.pdf
 popd
