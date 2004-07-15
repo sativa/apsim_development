41d6210998058f4c3d36e60aff91e3de5787c483
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-07-15 23:10:50 +0000

    Added APSFront & Howwet

--- trunk/makegui.bat
+++ trunk/makegui.bat
@@ -11,3 +11,21 @@ cmd /c %APSMAKE%  >> %APSROOT%\gui.out
 rem --------- Go build all gui stuff.
 cd %APSROOT%
 %APSROOT%\tools\segmake\segmake %APSROOT%\all.bpg
+
+rem --------- Build old APSFront
+echo ------Compiling  %APSROOT%\APSFront\source >> %APSROOT%\gui.out
+cd %APSROOT%
+path > p.bat
+path=c:\bc5\bin
+cd %APSROOT%\apsfront\source
+make -s -f apsfront.mak  >> %APSROOT%\gui.out
+
+rem --------- Build old HOWWET
+echo ------Compiling  %APSROOT%\Howwet\source >> %APSROOT%\gui.out
+cd %APSROOT%\howwet\source
+make -s -f howwet.mak >> %APSROOT%\gui.out
+
+rem --------- Restore path.
+cd %APSROOT%
+call p.bat
+del p.bat
