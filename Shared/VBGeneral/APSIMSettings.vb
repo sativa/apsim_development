2442d6c5363aa39992d646dfa761a12abab5dea3
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-08-10 03:41:09 +0000

    no message

--- Shared/VBGeneral/APSIMSettings.vb
+++ Shared/VBGeneral/APSIMSettings.vb
@@ -1,13 +1,13 @@
 Public Class APSIMSettings
     Declare Ansi Sub GetAPSIMSetting Lib "APSIMShared.dll" Alias "SettingsRead" (ByVal key As String, ByVal returnstring As String, ByVal replacemacros As Integer)
     Declare Ansi Sub SetAPSIMSetting Lib "APSIMShared.dll" Alias "SettingsWrite" (ByVal key As String, ByVal returnstring As String)
-    Private Const MaxStringLength = 255
+
     Public Function GetSetting(ByVal section As String, ByVal keyword As String) As String
         Try
-            Dim Setting As String = Space(MaxStringLength)
+            Dim Setting As String = Space(Utility.MaxStringLength)
             Dim key As String = Trim(section) + "|" + Trim(keyword)
             GetAPSIMSetting(key, Setting, 1) '1 is true in C++
-            GetSetting = CStringToVBString(Setting)
+            GetSetting = Utility.CStringToVBString(Setting)
         Catch ex As Exception
             MsgBox(ex.Message, MsgBoxStyle.Critical, "Error reading APSIM Setting")
         End Try
@@ -20,14 +20,5 @@ Public Class APSIMSettings
             MsgBox(ex.Message, MsgBoxStyle.Critical, "Error writting APSIM Setting")
         End Try
     End Sub
-    Private Function CStringToVBString(ByVal Cstring As String) As String
-        ' This function converts a C string to a vb string by returning everything
-        ' up to the null character
-        Try
-            Dim NullChar As New Char
-            CStringToVBString = Cstring.Substring(0, Cstring.IndexOf(NullChar))
-        Catch e As Exception
-            MsgBox("Error converting string types", MsgBoxStyle.Critical)
-        End Try
-    End Function
+
 End Class
