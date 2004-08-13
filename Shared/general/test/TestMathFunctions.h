a85ec3d881cb3b225b15e895703e3f575020af37
wangj <wangj@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-08-13 05:52:39 +0000

    unit testing

---
+++ Shared/general/test/TestMathFunctions.h
@@ -0,0 +1,18 @@
+// TestMath_functions.h - header for TestMath_functions.cpp
+//
+// J Wang
+// Aug 2004
+
+
+//---------------------------------------------------------------------------
+#ifndef TestMathFunctionsH
+#define TestMathFunctionsH
+
+#include <boost/test/unit_test.hpp>
+
+//---------------------------------------------------------------------------
+// Register all tests.
+//---------------------------------------------------------------------------
+boost::unit_test_framework::test_suite* testMathFunctions(void);
+
+#endif
---
+++ Shared/general/test/TestPathClass.cpp
@@ -0,0 +1,11 @@
+// TestPathClass.cpp - Test for the path class, using the Boost library
+//
+// J Wang
+// Aug 2004
+
+#include "../path.h"
+#include "TestPathClass.h"
+#include <boost/test/unit_test.hpp>
+using boost::unit_test_framework::test_suite;
+
+void Path_test::TestIsEmpty(){}
