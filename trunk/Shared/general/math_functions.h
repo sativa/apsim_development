33ee8be599ce11c37399e4de24ab2ccd9b5f815f
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-08-23 01:03:12 +0000

    Upgraded MINIM to use std::vector

--- trunk/Shared/general/math_functions.h
+++ trunk/Shared/general/math_functions.h
@@ -5,6 +5,50 @@
 #include <string>
 #include <algorith>
 #include <functional>
+// ------------------------------------------------------------------
+//  Short description:
+//    cycle through a number series from 1 to Max_number.
+//    returns the next number in the sequence.
+//    eg.  if current_number = 1   and max_number = 4 returns 1
+//         if current_number = 2   and max_number = 4 returns 2
+//         if current_number = 3   and max_number = 4 returns 3
+//         if current_number = 4   and max_number = 4 returns 4
+//         if current_number = 5   and max_number = 4 returns 1
+
+//  Notes:
+
+//  Changes:
+//    DPH 18/4/1997
+
+// ------------------------------------------------------------------
+int Cycle (int Current_number, int Max_number);
+
+
+struct Regr_stats
+   {
+   float m;
+   float c;
+   float SEslope;
+   float SEcoeff;
+   float R2;
+   float ADJR2;
+   float R2YX;
+   float VarRatio;
+   float RMSD;
+   };
+
+// ------------------------------------------------------------------
+//  Short description:
+//     calculate a regression from the specified data.
+
+//  Notes:
+
+//  Changes:
+//    DPH 18/4/1997
+
+// ------------------------------------------------------------------
+void calcRegressionStats (const std::vector<double>& x, const std::vector<double>& y,
+                          Regr_stats& stats);
 #include <boost\lexical_cast.hpp>
 #include <general\string_functions.h>  // ftoa()
 
