1095e06e5f4be3fb3add27268e65d8e8d514ca46
nightly_build <nightly_build@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2004-11-05 05:53:45 +0000

    *** empty log message ***

--- trunk/makedotnet.bat
+++ trunk/makedotnet.bat
@@ -1,5 +1,5 @@
 @echo off
-set APSROOT=c:\mydata\development
+set APSROOT=c:\development
 
 rem --------- Compile VBGeneral
 cd %APSROOT%\Shared\VBGeneral
@@ -11,6 +11,12 @@ rem --------- Compile C#General
 cd %APSROOT%\Shared\C#general
 echo ------Compiling  %APSROOT%\Shared\C#general >> %APSROOT%\dotnet.out
 "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" CSGeneral.csproj /rebuild debug >> %APSROOT%\dotnet.out
+cd %APSROOT%
+
+rem --------- Compile VBMet
+cd %APSROOT%\Shared\VBMet
+echo ------Compiling  %APSROOT%\Shared\VBMet >> %APSROOT%\dotnet.out
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" VBMet.csproj /rebuild debug >> %APSROOT%\dotnet.out
 cd %APSROOT%
 
 rem --------- Compile ApsimUI
