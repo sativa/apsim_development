3f0b20de7f41db74b639ee3222f7ff3cbaffebb7
devoilp <devoilp@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2005-03-02 04:30:52 +0000

    generates html outputs

--- apsim/Wheat/Validation/Validation.bat
+++ apsim/Wheat/Validation/Validation.bat
@@ -2,5 +2,5 @@ pushd
 cd \development\apsim\wheat\validation
 c:\progra~1\rems\rems.exe wheat.mdb crop.tem met.tem validation
 ..\..\..\bin\apsrun.exe /Q  validation.con  
-rem pause
+c:\progra~1\R\rw1090pat\bin\Rterm.exe -slave < validation-1.r
 popd
