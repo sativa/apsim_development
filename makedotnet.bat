e92ade033c81a524641e3372a390d89c423e94cc
nightly_build <nightly_build@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2005-08-05 04:09:13 +0000

    no message

--- makedotnet.bat
+++ makedotnet.bat
@@ -1,32 +1,8 @@
 @echo off
 set APSROOT=c:\development
 
-rem --------- Compile VBGeneral
-cd %APSROOT%\Shared\VBGeneral
-echo ------Compiling  %APSROOT%\Shared\VBGeneral > %APSROOT%\dotnet.out
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" VBGeneral.vbproj /rebuild debug >> %APSROOT%\dotnet.out
+rem --------- Compile All dot net stuff
 cd %APSROOT%
-
-rem --------- Compile C#General
-cd %APSROOT%\Shared\C#general
-echo ------Compiling  %APSROOT%\Shared\C#general >> %APSROOT%\dotnet.out
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" CSGeneral.csproj /rebuild debug >> %APSROOT%\dotnet.out
-cd %APSROOT%
-
-rem --------- Compile VBMet
-cd %APSROOT%\Shared\VBMet
-echo ------Compiling  %APSROOT%\Shared\VBMet >> %APSROOT%\dotnet.out
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" VBMet.vbproj /rebuild debug >> %APSROOT%\dotnet.out
-cd %APSROOT%
-
-rem --------- Compile ApsimUI
-cd %APSROOT%\ApsimUI\source
-echo ------Compiling  %APSROOT%\ApsimUI >> %APSROOT%\dotnet.out
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" ApsimUI.vbproj /rebuild debug >> %APSROOT%\dotnet.out
-cd %APSROOT%
-
-rem --------- Compile GetComponentDescription
-cd %APSROOT%\Tools\GetComponentDescription
-echo ------Compiling  %APSROOT%\Tools\GetComponentDescription >> %APSROOT%\dotnet.out
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" GetComponentDescription.csproj /rebuild debug >> %APSROOT%\dotnet.out
+echo ------Compiling  All dot net code > %APSROOT%\dotnet.out
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv" alldotnet.sln /rebuild debug >> %APSROOT%\dotnet.out
 cd %APSROOT%
