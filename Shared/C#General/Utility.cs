fd351634d511ce1d7b102571ad30f74d85106552
cov019 <cov019@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-03-06 04:33:13 +0000

    Added functions:
    EncodeStringToBase64
    EncodeBase64To String

--- Shared/C#General/Utility.cs
+++ Shared/C#General/Utility.cs
@@ -26,6 +26,45 @@ namespace CSGeneral
 			return new Bitmap(stream);
 			}
 
+		public static string EncodeBase64ToString(string base64String)
+		{
+			// Convert Base64 string back to a simmple string
+
+				// No memos then exit
+			if( base64String.Equals("") ) return "";
+
+
+			//Open up MemoryStream object to obtain an array of character bytes
+			System.IO.MemoryStream mem = new System.IO.MemoryStream( 
+					Convert.FromBase64String(base64String));
+
+			string str = ""; 
+			byte[] bite = mem.ToArray();
+
+			// Loop through array adding each character byte to the end of a string
+			foreach( byte abyte in bite)
+				str += Convert.ToChar(abyte);
+
+			//return formatted string
+			return str;
+
+		}
+
+		public static string EncodeStringToBase64(string str)
+		{
+			// Converts given string to Base64
+
+			System.IO.MemoryStream mem = new System.IO.MemoryStream(str.Length);
+
+			// Loop through each character in the memo, writing each to a MemoryStream buffer
+			foreach( char character in str.ToCharArray() )
+				mem.WriteByte(Convert.ToByte(character));
+
+			// convert byte array characters to Base64 return it.
+			return Convert.ToBase64String(mem.GetBuffer());
+
+		}
+
 		public static string ApplyStyleSheet(string Contents, string StylesheetContents)
 			{
 			//Create the XmlParserContext and reader.
