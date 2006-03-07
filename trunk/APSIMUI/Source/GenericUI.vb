c001a1f86b59129e9342c23c8dcba37e5c53cf88
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-03-07 04:37:30 +0000

    no message

--- trunk/APSIMUI/Source/GenericUI.vb
+++ trunk/APSIMUI/Source/GenericUI.vb
@@ -51,7 +51,7 @@ Public Class GenericUI
         Me.PictureBox.Dock = System.Windows.Forms.DockStyle.Left
         Me.PictureBox.Location = New System.Drawing.Point(0, 40)
         Me.PictureBox.Name = "PictureBox"
-        Me.PictureBox.Size = New System.Drawing.Size(88, 702)
+        Me.PictureBox.Size = New System.Drawing.Size(88, 511)
         Me.PictureBox.TabIndex = 3
         Me.PictureBox.TabStop = False
         '
@@ -62,7 +62,7 @@ Public Class GenericUI
         Me.FpSpread1.Location = New System.Drawing.Point(88, 40)
         Me.FpSpread1.Name = "FpSpread1"
         Me.FpSpread1.Sheets.AddRange(New FarPoint.Win.Spread.SheetView() {Me.Grid})
-        Me.FpSpread1.Size = New System.Drawing.Size(1144, 702)
+        Me.FpSpread1.Size = New System.Drawing.Size(824, 511)
         Me.FpSpread1.TabIndex = 4
         Me.FpSpread1.TabStripPolicy = FarPoint.Win.Spread.TabStripPolicy.Never
         '
@@ -75,6 +75,7 @@ Public Class GenericUI
         Me.Grid.ColumnHeader.Cells.Get(0, 0).Text = "Name"
         Me.Grid.ColumnHeader.Cells.Get(0, 1).Text = "Value"
         Me.Grid.Columns.Get(0).Label = "Name"
+        Me.Grid.Columns.Get(0).Locked = True
         Me.Grid.Columns.Get(0).Width = 206.0!
         Me.Grid.Columns.Get(1).Label = "Value"
         Me.Grid.Columns.Get(1).Width = 206.0!
@@ -88,7 +89,7 @@ Public Class GenericUI
         Me.Controls.Add(Me.FpSpread1)
         Me.Controls.Add(Me.PictureBox)
         Me.Name = "GenericUI"
-        Me.Size = New System.Drawing.Size(1232, 742)
+        Me.Size = New System.Drawing.Size(912, 551)
         Me.Controls.SetChildIndex(Me.PictureBox, 0)
         Me.Controls.SetChildIndex(Me.FpSpread1, 0)
         CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).EndInit()
