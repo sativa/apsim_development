8d8e710154edc66b4000219da20d0815e559c825
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-05-25 15:17:34 +0000

    To get around the Lahey Fortran compiler bug, I've changed to using a batch file instead of nested make files.

---
+++ apsim/Makefile.bat
@@ -0,0 +1,203 @@
+cd %APSROOT%\apsim\infra\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\croptemp\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\cropmod\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\accum\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\apswim\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\canopy\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\clock\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\correl\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\eo\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\erosion\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\fertiliz\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\frmrpt\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\grasp\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\growth\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\input\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\irrigate\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\log\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\maize\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\manager\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\map\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\micromet\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\millet\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\operatns\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\oryza\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\ozcot\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\parasite\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\PatchInput\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\Plant\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\report\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\SiloInput\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\SOI\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\soiln2\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\SoilP\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\soilpH\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\soilt\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\soiltemp\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\soilwat2\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\solute\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\sorghum\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\stock\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\sugar\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\SummaryFile\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\surface\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\SurfaceOM\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\Sysbal\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\tamet2\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\tcllink\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\Tracker\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\tree\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\VenLink\source
+echo ------Compiling %CD%
+%APSMAKE%
+
+cd %APSROOT%\apsim\WaterSupply\source
+echo ------Compiling %CD%
+%APSMAKE%
