2f11314c23b2ca77d102b7ca39d771e3b699448a
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-06-05 11:28:03 +0000

    Updated XCeed Component Licence Number

--- APSIMUI/Source/MainUI.vb
+++ APSIMUI/Source/MainUI.vb
@@ -68,7 +68,7 @@ Public Class MainUI
         splash.VersionText = "Version " & APSIMSettings.ApsimVersion
         splash.Show()
 
-        Xceed.Chart.Licenser.LicenseKey = "CHT30-YTL57-0UXLJ-145A"
+        Xceed.Chart.Licenser.LicenseKey = "CHT40-N4AAF-77UTD-6ANA"
 
         Application.DoEvents()
 
