ec630c7422219d0d0431bb6524e4fd527c4697a4
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-08-03 10:02:28 +0000

    Wheat validation no longer creates simulation files from REM with each build.

--- trunk/apsim/Wheat/Validation/Validation.bat
+++ trunk/apsim/Wheat/Validation/Validation.bat
@@ -1,6 +1,10 @@
 pushd
 cd \development\apsim\wheat\validation
-c:\progra~1\rems\rems.exe wheat.mdb crop.tem met.tem validation
-..\..\..\bin\apsrun.exe /auto  validation.con  
+
+rem no longer use rems to recreate simulation files with each build
+rem c:\progra~1\rems\rems.exe wheat.mdb crop.tem met.tem validation
+
+..\..\..\bin\apsrun.exe /auto  \development\apsim\wheat\validation\wheatvalidation.apsim  
+
 c:\progra~1\R\rw2010\bin\Rterm.exe --slave < validation-1.r
 popd
