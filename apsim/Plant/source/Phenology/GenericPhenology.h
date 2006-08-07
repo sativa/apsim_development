1e5bc1a9049fd9026d4d7b6d995dcfdafc61cd75
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-08-07 09:53:29 +0000

    Moved Phenology classes to their own folder

---
+++ apsim/Plant/source/Phenology/GenericPhenology.h
@@ -0,0 +1,27 @@
+#ifndef GenericPhenologyH
+#define GenericPhenologyH
+
+#include "CropPhenology.h"
+
+class GenericPhenology : public CropPhenology
+   {
+   protected:
+      float photoperiod;
+      virtual void setupTTTargets(void);
+
+   public:
+      GenericPhenology(PlantComponent *s, plantInterface *p) : CropPhenology(s, p) {};
+      void prepare(const environment_t &e);
+      void process(const environment_t &e, const pheno_stress_t &ps);
+      void readConstants (protocol::Component *, const string &);
+      void readSpeciesParameters (protocol::Component *, std::vector<string> &);
+      void readCultivarParameters (protocol::Component *, const string &);
+      void GenericPhenology::onSow(unsigned &, unsigned &, protocol::Variant &v);
+      void writeCultivarInfo (PlantComponent *);
+      void onRemoveBiomass(float removeBiomPheno);
+      virtual float TT(const environment_t &e);
+      float get_dlt_tt(void) {return dlt_tt;};
+      void zeroAllGlobals(void);
+   };
+
+#endif
