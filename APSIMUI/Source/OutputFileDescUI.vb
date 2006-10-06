81f5bcc8fe088ab3c7fc446bdf91790fd70e2469
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-10-06 15:42:56 +0000

    ApsimUI: Added move down and move up options to variables grid

--- APSIMUI/Source/OutputFileDescUI.vb
+++ APSIMUI/Source/OutputFileDescUI.vb
@@ -48,24 +48,32 @@ Public Class OutputFileDescUI
     Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
     Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
     Friend WithEvents Splitter1 As System.Windows.Forms.Splitter
+    Friend WithEvents ContextMenu As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents MoveUpMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MoveDownMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents Splitter2 As System.Windows.Forms.Splitter
 
     <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
         Dim TipAppearance1 As FarPoint.Win.Spread.TipAppearance = New FarPoint.Win.Spread.TipAppearance
         Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog
         Me.RightHandPanel = New System.Windows.Forms.Panel
+        Me.Spread = New FarPoint.Win.Spread.FpSpread
+        Me.ContextMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.MoveUpMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.MoveDownMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.Grid = New FarPoint.Win.Spread.SheetView
         Me.Splitter1 = New System.Windows.Forms.Splitter
         Me.VariableListView = New System.Windows.Forms.ListView
         Me.ColumnHeader1 = New System.Windows.Forms.ColumnHeader
         Me.ColumnHeader2 = New System.Windows.Forms.ColumnHeader
         Me.ColumnHeader4 = New System.Windows.Forms.ColumnHeader
         Me.ColumnHeader3 = New System.Windows.Forms.ColumnHeader
-        Me.Spread = New FarPoint.Win.Spread.FpSpread
-        Me.Grid = New FarPoint.Win.Spread.SheetView
         Me.Splitter2 = New System.Windows.Forms.Splitter
         Me.EventsListView = New APSIMUI.EventsListView
         Me.RightHandPanel.SuspendLayout()
         CType(Me.Spread, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenu.SuspendLayout()
         CType(Me.Grid, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -87,6 +95,69 @@ Public Class OutputFileDescUI
         Me.RightHandPanel.Name = "RightHandPanel"
         Me.RightHandPanel.Size = New System.Drawing.Size(753, 733)
         Me.RightHandPanel.TabIndex = 11
+        '
+        'Spread
+        '
+        Me.Spread.AccessibleDescription = "Spread, Sheet1, Row 0, Column 0, "
+        Me.Spread.AllowDrop = True
+        Me.Spread.ContextMenuStrip = Me.ContextMenu
+        Me.Spread.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Spread.EditModeReplace = True
+        Me.Spread.HorizontalScrollBarPolicy = FarPoint.Win.Spread.ScrollBarPolicy.AsNeeded
+        Me.Spread.Location = New System.Drawing.Point(0, 0)
+        Me.Spread.Name = "Spread"
+        Me.Spread.Sheets.AddRange(New FarPoint.Win.Spread.SheetView() {Me.Grid})
+        Me.Spread.Size = New System.Drawing.Size(753, 409)
+        Me.Spread.TabIndex = 14
+        TipAppearance1.BackColor = System.Drawing.SystemColors.Info
+        TipAppearance1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        TipAppearance1.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.Spread.TextTipAppearance = TipAppearance1
+        Me.Spread.VerticalScrollBarPolicy = FarPoint.Win.Spread.ScrollBarPolicy.AsNeeded
+        '
+        'ContextMenu
+        '
+        Me.ContextMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MoveUpMenuItem, Me.MoveDownMenuItem})
+        Me.ContextMenu.Name = "ContextMenu"
+        Me.ContextMenu.Size = New System.Drawing.Size(278, 70)
+        '
+        'MoveUpMenuItem
+        '
+        Me.MoveUpMenuItem.Name = "MoveUpMenuItem"
+        Me.MoveUpMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Up), System.Windows.Forms.Keys)
+        Me.MoveUpMenuItem.Size = New System.Drawing.Size(277, 22)
+        Me.MoveUpMenuItem.Text = "Move variables &up"
+        '
+        'MoveDownMenuItem
+        '
+        Me.MoveDownMenuItem.Name = "MoveDownMenuItem"
+        Me.MoveDownMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Down), System.Windows.Forms.Keys)
+        Me.MoveDownMenuItem.Size = New System.Drawing.Size(277, 22)
+        Me.MoveDownMenuItem.Text = "Move variables &down"
+        '
+        'Grid
+        '
+        Me.Grid.Reset()
+        'Formulas and custom names must be loaded with R1C1 reference style
+        Me.Grid.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.R1C1
+        Me.Grid.ColumnCount = 3
+        Me.Grid.AutoUpdateNotes = True
+        Me.Grid.ColumnHeader.Cells.Get(0, 0).Value = "Variable name"
+        Me.Grid.ColumnHeader.Cells.Get(0, 1).Value = "Array?"
+        Me.Grid.ColumnHeader.Cells.Get(0, 2).Value = "Description"
+        Me.Grid.Columns.Get(0).BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Grid.Columns.Get(0).Label = "Variable name"
+        Me.Grid.Columns.Get(0).Width = 145.0!
+        Me.Grid.Columns.Get(1).Label = "Array?"
+        Me.Grid.Columns.Get(1).Locked = True
+        Me.Grid.Columns.Get(1).Width = 52.0!
+        Me.Grid.Columns.Get(2).Label = "Description"
+        Me.Grid.Columns.Get(2).Locked = True
+        Me.Grid.Columns.Get(2).Width = 338.0!
+        Me.Grid.RowHeader.Columns.Default.Resizable = False
+        Me.Grid.RowHeader.Visible = False
+        Me.Grid.SheetName = "Sheet1"
+        Me.Grid.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.A1
         '
         'Splitter1
         '
@@ -130,48 +201,6 @@ Public Class OutputFileDescUI
         Me.ColumnHeader3.Text = "Description"
         Me.ColumnHeader3.Width = 410
         '
-        'Spread
-        '
-        Me.Spread.AccessibleDescription = "Spread, Sheet1, Row 0, Column 0, "
-        Me.Spread.AllowDrop = True
-        Me.Spread.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Spread.EditModeReplace = True
-        Me.Spread.HorizontalScrollBarPolicy = FarPoint.Win.Spread.ScrollBarPolicy.AsNeeded
-        Me.Spread.Location = New System.Drawing.Point(0, 0)
-        Me.Spread.Name = "Spread"
-        Me.Spread.Sheets.AddRange(New FarPoint.Win.Spread.SheetView() {Me.Grid})
-        Me.Spread.Size = New System.Drawing.Size(753, 409)
-        Me.Spread.TabIndex = 14
-        TipAppearance1.BackColor = System.Drawing.SystemColors.Info
-        TipAppearance1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        TipAppearance1.ForeColor = System.Drawing.SystemColors.InfoText
-        Me.Spread.TextTipAppearance = TipAppearance1
-        Me.Spread.VerticalScrollBarPolicy = FarPoint.Win.Spread.ScrollBarPolicy.AsNeeded
-        '
-        'Grid
-        '
-        Me.Grid.Reset()
-        'Formulas and custom names must be loaded with R1C1 reference style
-        Me.Grid.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.R1C1
-        Me.Grid.ColumnCount = 3
-        Me.Grid.AutoUpdateNotes = True
-        Me.Grid.ColumnHeader.Cells.Get(0, 0).Value = "Variable name"
-        Me.Grid.ColumnHeader.Cells.Get(0, 1).Value = "Array?"
-        Me.Grid.ColumnHeader.Cells.Get(0, 2).Value = "Description"
-        Me.Grid.Columns.Get(0).BackColor = System.Drawing.Color.WhiteSmoke
-        Me.Grid.Columns.Get(0).Label = "Variable name"
-        Me.Grid.Columns.Get(0).Width = 145.0!
-        Me.Grid.Columns.Get(1).Label = "Array?"
-        Me.Grid.Columns.Get(1).Locked = True
-        Me.Grid.Columns.Get(1).Width = 52.0!
-        Me.Grid.Columns.Get(2).Label = "Description"
-        Me.Grid.Columns.Get(2).Locked = True
-        Me.Grid.Columns.Get(2).Width = 338.0!
-        Me.Grid.RowHeader.Columns.Default.Resizable = False
-        Me.Grid.RowHeader.Visible = False
-        Me.Grid.SheetName = "Sheet1"
-        Me.Grid.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.A1
-        '
         'Splitter2
         '
         Me.Splitter2.Dock = System.Windows.Forms.DockStyle.Bottom
@@ -201,6 +230,7 @@ Public Class OutputFileDescUI
         Me.Controls.SetChildIndex(Me.RightHandPanel, 0)
         Me.RightHandPanel.ResumeLayout(False)
         CType(Me.Spread, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenu.ResumeLayout(False)
         CType(Me.Grid, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -336,6 +366,7 @@ Public Class OutputFileDescUI
                     Grid.Cells(e.Row, 2).Text = Item.SubItems(3).Text
                 End If
             Next
+            Controller.DirtyData = True
             UserChange = True
         End If
     End Sub
@@ -372,5 +403,49 @@ Public Class OutputFileDescUI
 
 #End Region
 
+    Private Function CanMoveUp() As Boolean
+        If Grid.SelectionCount = 1 Then
+            Dim Selection As FarPoint.Win.Spread.Model.CellRange = Grid.GetSelection(0)
+            Return Selection.Row > 0
+        Else
+            Return False
+        End If
+    End Function
 
+    Private Sub MoveUpMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MoveUpMenuItem.Click
+        ' Move the 1 row from in front of current selection to one row just after the current selection
+        If CanMoveUp() Then
+            UserChange = False
+            Dim Selection As FarPoint.Win.Spread.Model.CellRange = Grid.GetSelection(0)
+            Dim FromRow As Integer = Selection.Row - 1
+            Dim ToRow As Integer = Selection.Row + Selection.RowCount
+            Grid.AddRows(ToRow, 1)
+            For Col As Integer = 0 To Grid.ColumnCount - 1
+                Grid.Cells(ToRow, Col).Text = Grid.Cells(FromRow, Col).Text
+            Next
+            Grid.RemoveRows(FromRow, 1)
+            Grid.ClearSelection()
+            Grid.AddSelection(Selection.Row - 1, Selection.Column, Selection.RowCount, Selection.ColumnCount)
+            Controller.DirtyData = True
+            UserChange = True
+        End If
+    End Sub
+
+    Private Sub MoveDownMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MoveDownMenuItem.Click
+        ' Move the 1 row from in front of current selection to one row just after the current selection
+        UserChange = False
+        Dim Selection As FarPoint.Win.Spread.Model.CellRange = Grid.GetSelection(0)
+        Dim FromRow As Integer = Selection.Row + Selection.RowCount
+        Dim ToRow As Integer = Selection.Row
+        Grid.AddRows(ToRow, 1)
+        FromRow += 1
+        For Col As Integer = 0 To Grid.ColumnCount - 1
+            Grid.Cells(ToRow, Col).Text = Grid.Cells(FromRow, Col).Text
+        Next
+        Grid.RemoveRows(FromRow, 1)
+        Grid.ClearSelection()
+        Grid.AddSelection(Selection.Row + 1, Selection.Column, Selection.RowCount, Selection.ColumnCount)
+        Controller.DirtyData = True
+        UserChange = True
+    End Sub
 End Class
