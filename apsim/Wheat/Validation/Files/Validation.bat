6e67462019dda9599bb09a48f77a92854c944e73
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-11-07 11:49:27 +0000

    Rearranged the wheat validation stuff so that graphs go directly under the validation directory.

---
+++ apsim/Wheat/Validation/Files/Validation.bat
@@ -0,0 +1,18 @@
+pushd
+cd \development\apsim\wheat\validation
+
+\development\bin\apsrun.exe /auto  \development\apsim\wheat\validation\files\wheatvalidation.apsim  
+
+c:\progra~1\R\rw2010\bin\Rterm.exe --slave < validation-1.r
+
+\development\bin\apsimreport.exe aps06.report ..\aps6\aps06.gif
+\development\bin\apsimreport.exe aps06summary.report ..\aps6\aps06summary.gif
+\development\bin\apsimreport.exe wheatvalidation.report ..\wheatvalidation.gif
+
+pushd
+cd yieldprophet\2005
+call runall.bat
+popd
+\development\bin\apsimreport.exe YieldProphet2005.report ..\YieldProphet2005.gif
+
+popd
