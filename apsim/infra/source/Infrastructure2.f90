f163c6e86a2e601d59e01f8c4be8d8ee733ccb0e
devoilp <devoilp@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2006-12-04 02:22:57 +0000

    added fortran wrapper to CI2

---
+++ apsim/infra/source/Infrastructure2.f90
@@ -0,0 +1,15 @@
+module Infrastructure2
+   use ConstantsModule
+   use ErrorModule
+   use DataModule
+   use DataStrModule
+   use DateModule
+   use ScienceModule
+   use StringModule
+   use ComponentInterface2Module
+   use ConvertModule
+   use Read2Module
+   use Get2Module
+   use DataTypes
+   use DataTypesInterface
+end module Infrastructure2
