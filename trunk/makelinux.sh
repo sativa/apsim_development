58b76db4222498099c0b0a78b03da3e59af2c839
devoilp <devoilp@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-02-14 15:28:13 +0000

    unix makefile changes for pioneer

--- trunk/makelinux.sh
+++ trunk/makelinux.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 export APSROOT=`pwd`
+
+#export APSROOT=/psfs/enclass2/DevelopmentLinux/APSIM/
+#export BOOST_INCLUDEDIR-I/usr/local/include/boost-1_33_1
+#export XML2_INCLUDEDIR=-I/usr/local/libxml2-2.6.23/include/libxml2
+#export XML2_LIBDIR=-L/usr/local/libxml2-2.6.23/lib
+
 export LD_LIBRARY_PATH=$APSROOT/lib
 ulimit -s unlimited
 
 make -f Makefile.linux clobber 
 make -f Makefile.linux install 
-
