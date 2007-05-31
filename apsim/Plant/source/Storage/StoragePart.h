58c67c7312733324d4d93608a00de6875b4cdc0c
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-05-31 04:49:14 +0000

    Fixes for potato model - tuber stuff and surfaceom specs

--- apsim/Plant/source/Storage/StoragePart.h
+++ apsim/Plant/source/Storage/StoragePart.h
@@ -18,6 +18,8 @@ class StoragePart : public plantPart
                      vector<float> &fraction_to_residue);
       void  update(void);
       void  removeBiomass2(float);
+   float dmGreenDemand(void) const
+     { return(1e6);};   // Maximum DM this part can take today - ask for all you can get - need to do this better
    };
 
 #endif /* StoragePartH */
