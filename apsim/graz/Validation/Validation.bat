7a6c5533a7927689ce915dc55768a996f5583828
devoilp <devoilp@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-07-10 02:21:00 +0000

    batch file for grazing validation runs

---
+++ apsim/graz/Validation/Validation.bat
@@ -0,0 +1,8 @@
+pushd
+cd \development\apsim\graz\Validation
+
+\development\bin\apsrun.exe /auto  \development\apsim\graz\Validation\BSc4past.con
+
+REM c:\progra~1\R\R-2.4.0\bin\Rterm.exe --slave < validation-1.r
+
+popd
