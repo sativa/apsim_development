0a746649b6b9b4daa814f27ecb8b8621c7cac81a
devoilp <devoilp@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-07-24 05:27:05 +0000

    silo input module uses current working directory for temporary files

--- trunk/apsim/SiloInput/source/SiloInputComponent.cpp
+++ trunk/apsim/SiloInput/source/SiloInputComponent.cpp
@@ -1,20 +1,19 @@
 #include <fstream>
 #include <stdexcept>
 #include <string>
-#include <sys/stat.h>
-
 #ifdef __WIN32__
+ #include <dir.h>
  #include <process.h>
-// #include <dos.h>
-// #define sleep _sleep
+ #define PATH_MAX 1024
 #else
  #include <unistd.h>
 #endif
 
+#include <sys/stat.h>
+
 #include <boost/date_time/gregorian/gregorian.hpp>
 
 #include <general/string_functions.h>
-#include <general/path.h>
 #include <general/http.h>
 
 #include <ApsimShared/ApsimDataFile.h>
@@ -39,8 +38,6 @@ protocol::Component* createComponent(void)
 // ------------------------------------------------------------------
 SiloInputComponent::SiloInputComponent(void)
    {
-   unsigned int pid = getpid();
-   fileName = Path::getTempFolder().Get_path() + string("/temp") + itoa(pid) + string(".met");
    stationNumber = 0;
    }
 // ------------------------------------------------------------------
@@ -64,19 +61,27 @@ void SiloInputComponent::doInit2(void)
 // ------------------------------------------------------------------
 void SiloInputComponent::openInputFile(void)
    {
-   readParameter ("parameters", "station_number", stationNumber, 0, 100000);
+   // Find a unique temporary name to hold our data
+   unsigned int pid = getpid();
 
+   char wd[PATH_MAX];
+   getcwd(wd, PATH_MAX);
+   fileName = string(wd) + string("/temp") + itoa(pid) + string(".met");
+
+   // Now the SILO station number 
+   readParameter ("parameters", "station_number", stationNumber, 0, 100000);
    string requestString =
       string("http://192.168.0.60/cgi-bin/getData.tcl?format=apsim&station=") +
       itoa(stationNumber) +
       string("&ddStart=1&mmStart=1&yyyyStart=1800&ddFinish=31&mmFinish=12&yyyyFinish=2100");
 
    tHTTP http;
-
    bool ok = http.Get(fileName, requestString);
-
-   if (!ok) {throw std::runtime_error(http.ErrorMessage());}
-
+   
+   // HTTP connection errors are caught here (eg webserver down / unreachable)
+   if (!ok) {throw std::runtime_error(http.ErrorMessage());}    
+   
+   // But you still may get a zero-sized reponse, eg invalid station number.
    struct stat statbuf;
    if (stat(fileName.c_str(), &statbuf) < 0) 
        throw std::runtime_error("Temporary met file " + fileName + " is missing");
