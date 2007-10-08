539a053c9598f46da2462ccc7a7f3f9bac448e25
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-10-08 04:25:30 +0000

    Reordered Plant.Pool variable registrations from GreenLeafWt to LeafGreenWt.

--- apsim/Plant/source/Delta.cpp
+++ apsim/Plant/source/Delta.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 Delta::Delta(ScienceAPI& scienceAPI, const std::string& Name, const std::string& PartName)
    {
-   scienceAPI.expose("Delta"+Name+PartName+"Wt", "g/m^2", "Change in " + Name + " " + PartName + " dry matter", DM);
-   scienceAPI.expose("Delta"+Name+PartName+"N",  "g/m^2", "Change in " + Name + " " + PartName + " nitrogen", N);
-   scienceAPI.expose("Delta"+Name+PartName+"P",  "g/m^2", "Change in " + Name + " " + PartName + " phosphorus", P);
+   scienceAPI.expose(PartName+"Delta"+Name+"Wt", "g/m^2", "Change in " + Name + " " + PartName + " dry matter", DM);
+   scienceAPI.expose(PartName+"Delta"+Name+"N",  "g/m^2", "Change in " + Name + " " + PartName + " nitrogen", N);
+   scienceAPI.expose(PartName+"Delta"+Name+"P",  "g/m^2", "Change in " + Name + " " + PartName + " phosphorus", P);
    }
 
 void Delta::Move (Pool& From, Pool& To)
