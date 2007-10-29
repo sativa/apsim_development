036062df2ad067e9f64e0e3bb3b7645a01c5f6f3
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-10-29 10:46:05 +0000

    Removed the validation APSIM runs out of the validation batch files.

--- trunk/apsim/lupin/Validation/Validation.bat
+++ trunk/apsim/lupin/Validation/Validation.bat
@@ -1,10 +1,6 @@
 pushd
 cd %APSROOT%\apsim\lupin\validation
-
-%APSROOT%\bin\apsrun.exe /auto  %APSROOT%\apsim\lupin\validation\validation.con  
 %APSROOT%\bin\apsimreport.exe validation.report LupinValidation.gif
-
 mkdir ..\sensibility
 %APSROOT%\bin\apsimreport.exe sensibility.report ..\sensibility\LupinSensibility.gif
-
 popd
