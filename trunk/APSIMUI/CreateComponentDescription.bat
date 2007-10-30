b8b3af8434cd77e6b6cebd44e145d3764b8f719c
har297 <har297@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-10-30 10:26:42 +0000

    Changed Farmwise to Ausfarm

--- trunk/APSIMUI/CreateComponentDescription.bat
+++ trunk/APSIMUI/CreateComponentDescription.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 echo ^<?xml version="1.0"?^> > %APSROOT%\ApsimUI\ComponentDescription.xml
 echo ^<?xml-stylesheet type="text/xsl" href="../docs/shared/Variables.xsl"?^> >> %APSROOT%\ApsimUI\ComponentDescription.xml
@@ -12,10 +12,10 @@ echo ^<ComponentDescription^> >> %APSROOT%\ApsimUI\ComponentDescription.xml
 %APSROOT%\bin\getcomponentdescription %APSROOT%\apsim\cropmod\lib\cropmod.dll sunflower >> %APSROOT%\ApsimUI\ComponentDescription.xml
 
 c:
-cd "c:\program files\farmwise"
-%APSROOT%\bin\getcomponentdescription "c:\program files\farmwise\stock.dll" stock >> %APSROOT%\ApsimUI\ComponentDescription.xml
-%APSROOT%\bin\getcomponentdescription "c:\program files\farmwise\pasture.dll" pasture >> %APSROOT%\ApsimUI\ComponentDescription.xml
-%APSROOT%\bin\getcomponentdescription "c:\program files\farmwise\supplement.dll" supplement >> %APSROOT%\ApsimUI\ComponentDescription.xml
+cd "c:\program files\ausfarm"
+%APSROOT%\bin\getcomponentdescription "c:\program files\ausfarm\stock.dll" stock >> %APSROOT%\ApsimUI\ComponentDescription.xml
+%APSROOT%\bin\getcomponentdescription "c:\program files\ausfarm\pasture.dll" pasture >> %APSROOT%\ApsimUI\ComponentDescription.xml
+%APSROOT%\bin\getcomponentdescription "c:\program files\ausfarm\supplement.dll" supplement >> %APSROOT%\ApsimUI\ComponentDescription.xml
 echo ^</ComponentDescription^> >> %APSROOT%\ApsimUI\ComponentDescription.xml
 
 
