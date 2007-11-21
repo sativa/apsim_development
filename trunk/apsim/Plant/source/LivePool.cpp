331d2ff89d766de451e472e74faa98d608eea002
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2007-11-21 22:38:57 +0000

    Plant: 2 small changes to get it to compile

--- trunk/apsim/Plant/source/LivePool.cpp
+++ trunk/apsim/Plant/source/LivePool.cpp
@@ -8,8 +8,9 @@
 using namespace std;
 
 LivePool::LivePool(ScienceAPI& API, const std::string& Name, const std::string& PartName)
+   : Pool(API, Name, PartName)
    {
-   Pool::Pool(API, Name, PartName);
+   
 
     n_conc_crit.read(API
                         , "x_stage_code" , "()", 1.0, 100.0
