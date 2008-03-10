ea811f5618c94e8f18880ab78ce77f7e339d738d
hut104 <hut104@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2008-03-10 03:57:23 +0000

    Fixed bug in ordering of photosynthesis model parameter reading.

--- apsim/Plant/source/Photosynthesis/RUEModel.cpp
+++ apsim/Plant/source/Photosynthesis/RUEModel.cpp
@@ -4,9 +4,9 @@ using namespace std;
 RUEModel::RUEModel(ScienceAPI& scienceAPI, plantInterface& p)
    : PhotosynthesisModel(scienceAPI, p)
    {
-    RUE.read(scienceAPI,
-              "x_stage_rue", "()", 0.0, 1000.0,
-              "y_rue", "(g dm/mj)", 0.0, 1000.0);
+//    RUE.read(scienceAPI,
+//              "x_stage_rue", "()", 0.0, 1000.0,
+//              "y_rue", "(g dm/mj)", 0.0, 1000.0);
    };
 
 float RUEModel::Potential (float radiationInterceptedGreen)
@@ -15,4 +15,11 @@ float RUEModel::Potential (float radiationInterceptedGreen)
                                , plant.getOxdefPhoto()), plant.getPfactPhoto());
 
    return radiationInterceptedGreen  * RUE.value(plant.getStageNumber())* stress_factor;
+   }
+
+void RUEModel::Read(void)
+   {
+    RUE.read(scienceAPI,
+              "x_stage_rue", "()", 0.0, 1000.0,
+              "y_rue", "(g dm/mj)", 0.0, 1000.0);
    }
