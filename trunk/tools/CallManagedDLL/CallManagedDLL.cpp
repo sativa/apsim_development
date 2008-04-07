0257f37e6ac11a2ebf6ed39f3f05f95e2f0dd6a5
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2008-04-07 06:03:52 +0000

    Added a mechanism to CallManagedDLL to allow FORTRAN and c++ to call into .NET classes. This can be used to migrate legacy code to .NET in an iterative fashion.

--- trunk/tools/CallManagedDLL/CallManagedDLL.cpp
+++ trunk/tools/CallManagedDLL/CallManagedDLL.cpp
@@ -4,6 +4,7 @@
 using namespace System;
 using namespace System::Reflection;
 using namespace System::Windows::Forms;
+using namespace System::Collections::Generic;
 
 	void CallDLLInternal(String^ fileName, String^ className, String^ methodName, String^ methodArgument)
 		{	
@@ -35,3 +36,51 @@ extern "C" __declspec(dllexport) void __stdcall  CallDLL(const char* dllFileName
 	{
 	CallDLLInternal(gcnew String(dllFileName), gcnew String(className), gcnew String(methodName), gcnew String(methodArgument));
 	}
+	
+int Instances::CreateClass(String^ dllFileName, String^ className)
+	{
+	String^ dll = gcnew String(dllFileName);
+	if (System::IO::File::Exists(dll))
+		{
+		Assembly^ assembly = Assembly::LoadFrom(gcnew String(dllFileName));
+		array<Type^>^ Types = assembly->GetTypes();
+		System::Collections::IEnumerator^ myEnum = Types->GetEnumerator();
+		while (myEnum->MoveNext())
+			{
+			Type^ oType = safe_cast<Type^>(myEnum->Current);
+			if (oType->Name == className)
+				{
+				Object^ o = Activator::CreateInstance(oType);
+				objects->Add(o);
+				return objects->Count-1;
+				}
+			}
+		}
+	else
+		{
+		String^ msg = gcnew String("Cannot find dll: " + dll);
+		System::Windows::Forms::MessageBox::Show(msg, "Error", System::Windows::Forms::MessageBoxButtons::OK);
+		return -1;
+		}
+	}
+
+void Instances::CallMethod(unsigned Index, const char* methodName, Object^ argument)
+	{
+	String^ MethodName = gcnew String(methodName);
+	Object^ o = objects[Index];
+	MethodInfo^ Method = o->GetType()->GetMethod(MethodName);
+	if (Method == nullptr)
+		MessageBox::Show("Cannot find method '" + MethodName + "' in .NET class");
+	else
+		{
+		array<Object^>^ methodArgs = gcnew array<Object^>(1);
+		methodArgs[0] = argument;
+		Method->Invoke(o, methodArgs);
+		}
+	}
+
+extern "C" __declspec(dllexport) int __stdcall  CreateClass(const char* dllFileName, const char* className)
+	{
+	return Instances::CreateClass(gcnew String(dllFileName), gcnew String(className));
+	}
+	
