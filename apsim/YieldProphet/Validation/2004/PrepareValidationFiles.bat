4459e6721a3335d09ae8cfc016496cf0ee82d1ea
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2008-07-03 06:21:34 +0000

    Some changes to the YieldProphet validation to get it to run on Bob

--- apsim/YieldProphet/Validation/2004/PrepareValidationFiles.bat
+++ apsim/YieldProphet/Validation/2004/PrepareValidationFiles.bat
@@ -7,22 +7,22 @@ rem Extract the necessary paddocks from PaddockDetails.xml and put
 rem into YieldProphet.xml. The names to extract are listed in 
 rem PaddockNames.txt
 rem --------------------------------------------------------------
-cd %APSROOT%\Tools\ExtractXMLElements\bin\Debug\
+cd %APSROOT%\Tools\ExtractXMLElements\bin\Release\
 ExtractXMLElements.exe %HERE%\PaddockDetails.xml %HERE%\PaddockNames.txt %HERE%\YieldProphet.xml
 
 rem --------------------------------------------------------------
 rem Turn the YieldProphet.xml file, created in the previous step,
 rem into a YieldProphet.apsim file that is ready to run
 rem --------------------------------------------------------------
-cd c:\hol353\ApsimWork\YieldProphet\ProcessYPDirectory\bin\Debug
-ProcessYPDirectory.exe %HERE%
+cd %APSROOT%\Apsim\YieldProphet\ProcessYPDirectory\bin\Release
+ProcessYPDirectory.exe %HERE% %HERE%\..\Template.apsim
 
 rem --------------------------------------------------------------
 rem Remove all references to SILO from the YieldProphet.apsim file
 rem that was created in the previous step. NB the year on the
 rem end of the next line.
 rem --------------------------------------------------------------
-cd %APSROOT%\Tools\ConvertSimsFromSILOToMetFile\bin\Debug
+cd %APSROOT%\Tools\ConvertSimsFromSILOToMetFile\bin\Release
 del /Q %HERE%\*.met
 ConvertSimsFromSILOToMetFile.exe %HERE%\YieldProphet.apsim 2004
 
