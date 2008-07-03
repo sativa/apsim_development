9d0f65653fcf68e157439019befe598623bfce20
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2008-07-03 04:09:07 +0000

    New YieldProphet validation + a rename of YPCalc module - part #2

---
+++ apsim/YieldProphet/Validation/2005/Validation.bat
@@ -0,0 +1,32 @@
+@echo off
+
+set HERE=%CD%
+set APSROOT=%CD%\..\..\..\..\
+
+rem -------------------------------------------------------------------
+rem Run APSIM on all simulations.
+rem -------------------------------------------------------------------
+%APSROOT%\bin\apsrun /auto %HERE%\YieldProphet.apsim
+
+rem -------------------------------------------------------------------
+rem Merge all outputs files into a single file.
+rem -------------------------------------------------------------------
+cd %APSROOT%\tools\MergeOutputFiles\bin\Debug
+MergeOutputFiles %HERE%\*Yearly.out %HERE%\All.txt
+
+rem -------------------------------------------------------------------
+rem Clean up all unwanted files.
+rem -------------------------------------------------------------------
+cd %HERE%
+del /Q *.sum
+del /Q *.sim
+del /Q *.out
+
+rem -------------------------------------------------------------------
+rem Rename All.txt to All.out. Couldn't do this earlier as it would
+rem have been deleted.
+rem -------------------------------------------------------------------
+ren All.txt All.out
+
+set HERE=
+set APSROOT=
---
+++ apsim/YieldProphet/Validation/2006/PrepareValidationFiles.bat
@@ -0,0 +1,31 @@
+@echo off
+set HERE=%CD%
+set APSROOT=%CD%\..\..\..\..\
+
+rem --------------------------------------------------------------
+rem Extract the necessary paddocks from PaddockDetails.xml and put
+rem into YieldProphet.xml. The names to extract are listed in 
+rem PaddockNames.txt
+rem --------------------------------------------------------------
+cd %APSROOT%\Tools\ExtractXMLElements\bin\Debug\
+ExtractXMLElements.exe %HERE%\PaddockDetails.xml %HERE%\PaddockNames.txt %HERE%\YieldProphet.xml
+
+rem --------------------------------------------------------------
+rem Turn the YieldProphet.xml file, created in the previous step,
+rem into a YieldProphet.apsim file that is ready to run
+rem --------------------------------------------------------------
+cd c:\hol353\ApsimWork\YieldProphet\ProcessYPDirectory\bin\Debug
+ProcessYPDirectory.exe %HERE%
+
+rem --------------------------------------------------------------
+rem Remove all references to SILO from the YieldProphet.apsim file
+rem that was created in the previous step. NB the year on the
+rem end of the next line.
+rem --------------------------------------------------------------
+cd %APSROOT%\Tools\ConvertSimsFromSILOToMetFile\bin\Debug
+del /Q %HERE%\*.met
+ConvertSimsFromSILOToMetFile.exe %HERE%\YieldProphet.apsim 2006
+
+set HERE=
+set APSROOT=
+pause
