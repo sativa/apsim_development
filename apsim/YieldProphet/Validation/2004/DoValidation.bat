867fff20e08039cee35f9ab63ce5454f58ae7fcd
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2008-07-21 01:45:37 +0000

    Removed the running of APSIM from the Yield Prophet dovalidation.bat files

--- apsim/YieldProphet/Validation/2004/DoValidation.bat
+++ apsim/YieldProphet/Validation/2004/DoValidation.bat
@@ -4,11 +4,6 @@ set HERE=%CD%
 set APSROOT=%CD%\..\..\..\..\
 
 rem -------------------------------------------------------------------
-rem Run APSIM on all simulations.
-rem -------------------------------------------------------------------
-%APSROOT%\bin\apsrun /auto %HERE%\YieldProphet.apsim
-
-rem -------------------------------------------------------------------
 rem Merge all outputs files into a single file.
 rem -------------------------------------------------------------------
 cd %APSROOT%\tools\MergeOutputFiles\bin\Release
