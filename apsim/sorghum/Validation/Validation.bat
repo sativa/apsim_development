afbbb9c47519956c6318864e280d569fac02f82a
hol353 <hol353@8bb03f63-af10-0410-889a-a89e84ef1bc8>
2008-08-19 02:24:27 +0000

    Sorghum validation from Greg

--- apsim/sorghum/Validation/Validation.bat
+++ apsim/sorghum/Validation/Validation.bat
@@ -1,4 +1,5 @@
 set APSBIN=c:\development\Bin
+set APSROOT=c:\development
 
 pushd
 cd %APSROOT%\apsim\Sorghum\Validation
@@ -21,33 +22,34 @@ cd .\Hermitage\HE1-8
 
 "%APSBIN%\apsimreport.exe" HE8.report ..\..\HE8.gif
 
-cd ..\..\Icrisat\BW5
+cd ..\..\Icrisat\BW
 
 "%APSBIN%\apsimreport.exe" BW5GxET1-4.report ..\..\BW5GxET1-4.gif
 "%APSBIN%\apsimreport.exe" BW5GxET5-8.report ..\..\BW5GxET5-8.gif
 "%APSBIN%\apsimreport.exe" BW5GxET9-12.report ..\..\BW5GxET9-12.gif
+"%APSBIN%\apsimreport.exe" BW8GxET1-4.report ..\..\BW8GxET1-4.gif
+"%APSBIN%\apsimreport.exe" BW8GxET5-8.report ..\..\BW8GxET5-8.gif
+"%APSBIN%\apsimreport.exe" BW8GxET9-12.report ..\..\BW8GxET9-12.gif
 
-cd ..\..\Lawes\LE13
+cd ..\..\Lawes\LE8
 "%APSBIN%\apsimreport.exe" Sorghum_LE13_Buster.report ..\..\LE13_Buster.gif
 "%APSBIN%\apsimreport.exe" Sorghum_LE13_M35-1.report ..\..\LE13_M35-1.gif
-
-cd ..\LE14
 "%APSBIN%\apsimreport.exe" Sorghum_LE14.report ..\..\LE14.gif
-
-cd ..\LE15
-"%APSBIN%\apsimreport.exe" Sorghum_LE15.report ..\..\LE15.gif
-
-cd ..\LE17
 "%APSBIN%\apsimreport.exe" Sorghum_LE17.report ..\..\LE17.gif
-
-cd ..\LE19
 "%APSBIN%\apsimreport.exe" LE19_Buster.report ..\..\LE19_Buster.gif
 "%APSBIN%\apsimreport.exe" LE19_CSH13R.report ..\..\LE19_CSH13R.gif
-
-cd ..\LE21
 "%APSBIN%\apsimreport.exe" LE21_A35xQL36.report  ..\..\LE21_A35xQL36.gif
 "%APSBIN%\apsimreport.exe" LE21_CSH13R.report    ..\..\LE21_CSH13R.gif
 "%APSBIN%\apsimreport.exe" LE21_QL39xQL36.report ..\..\LE21_QL39xQL36.gif
+
+
+cd %APSROOT%\apsim\Sorghum\Validation
+
+copy /Y Hermitage\HE1-8\*.out Predicted
+copy /Y Icrisat\BW\*.out Predicted
+copy /Y Lawes\LE\*.out Predicted
+
+"%APSBIN%\apsimreport.exe" SorghumValidation.report SorghumValidation.gif
 
 cd ..\..
 popd
